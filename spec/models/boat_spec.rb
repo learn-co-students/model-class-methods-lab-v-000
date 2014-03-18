86866dd0d829be817ee33213c2589e3c0d9a8b46
Arel English <arelenglish@gmail.com>
2014-03-18 19:05:03 -0400

    finished adding tests to Boat model

--- spec/models/boat_spec.rb
+++ spec/models/boat_spec.rb
@@ -26,28 +26,27 @@ describe Boat do
     it "returns all boats that are sailboats" do
       boats = ["H 28", "Nacra 17", "49er", "Laser", "Harpoon 4.7", "Sunfish"]
       expect(Boat.sailboats.pluck(:name)).to eq(boats)
-      # Boat.includes(:classifications).where(classifications: { name: 'Sailboat' }).pluck(:name).uniq
     end
   end
 
   describe "#with_three_classifications" do
     it "returns boats with three classifications" do
-
+      boats = ["Nacra 17", "Zodiac CZ7"]
+      expect(Boat.with_three_classifications.pluck(:name)).to eq(boats)
     end
   end
 
   describe "#last_three_alphabetically" do
     it "returns last three boats in alphabetical order" do
       boats = ["Zodiac CZ7", "Triton 21 TRX", "Sunfish", "Sun Tracker Regency 254 XP3", "Regulator 34SS"]
-      expect(Boat.last_three_alphabetically).to eq(boats)
+      expect(Boat.last_three_alphabetically.pluck(:name)).to eq(boats)
     end
   end
 
   describe "#without_a_captain" do
     it "returns boats without a captain" do
       boats = ["Harpoon 4.7", "Sunfish"]
-      expect(Boat.without_a_captain).to eq(boats)
-      # Boat.where(captain_id: nil).pluck(:name).uniq
+      expect(Boat.without_a_captain.pluck(:name)).to eq(boats)
     end
   end
 end
