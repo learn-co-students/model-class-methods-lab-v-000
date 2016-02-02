f2184555aa5eff2a29905f96e7fcc50fe545031f
frankNowinski <nowinski.frank@gmail.com>
2016-02-01 21:11:55 -0500

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,24 +3,17 @@ class Boat < ActiveRecord::Base
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
 
+  scope :dinghy, -> { where('length < 20') }
+  scope :ship, -> { where('length >= 20') }
+  scope :without_a_captain, -> { where(captain_id: nil) }
+
+
   def self.first_five
     first(5)
   end
 
-  def self.dinghy
-    where('length < 20')
-  end
-
-  def self.ship
-    where('length >= 20')
-  end
-
   def self.last_three_alphabetically
     order(name: :desc).first(3)
-  end
-
-  def self.without_a_captain
-    where(captain_id: nil)
   end
 
   def self.sailboats
