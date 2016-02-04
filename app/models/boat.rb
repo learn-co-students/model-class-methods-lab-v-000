1e58aa41b3ac593dae1f0dcf99537e952a0fc580
John Anthony Rivera <askinganthony@gmail.com>
2016-02-03 22:48:35 -0800

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,39 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+  def self.first_five
+  	self.all.limit(5)
+  end
+
+  def classification_names
+    self.classifications.map{|cls| cls.name}
+  end
+
+  def self.dinghy
+    self.all.where('length <20')
+  end
+
+  def self.ship
+    self.all.where('length >20')
+  end
+
+  def self.last_three_alphabetically
+    self.all.order('name desc').limit(3)
+  end
+
+  def self.without_a_captain
+    self.all.where(captain: nil)
+  end
+
+  def self.sailboats
+    self.all.find_all do |boat|
+      boat.classifications.any?{|c| c.name=="Sailboat"}
+    end
+  end
+
+  def self.with_three_classifications
+    self.all.find_all{|boat| boat.classifications.count==3}
+  end
+
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  ###Just realized I was supposed to be using AREL this whole time .-.
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+
+  def self.sailors
+  	includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def self.talented_seamen
+    motorboat_owners=includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+    sailboat_owners=includes(boats: :classifications).where(classifications: {name: "Sailboat"})
+    motorboat_owners & sailboat_owners #This one was mind-meltingly difficult for me, and now it seems so simple
+  end
+
+  def self.non_sailors
+    self.all-self.sailors
+  end
+
+
 end
