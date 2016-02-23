b71c8b5000e09bacc040cbc9ae5ab7b60d789870
Kevin McCormack <wittyman37@yahoo.com>
2016-02-22 23:08:14 -0500

    finished

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -10,18 +10,23 @@ class Captain < ActiveRecord::Base
     includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
   end
 
+  def self.motorboaters
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+  end
+
   def self.talented_seamen
     #binding.pry
     #includes(boats: :classifications).where("classification.name = Motorboat AND classification.name = Sailboat").uniq
     #includes(boats: :classifications).where(classifications: {name: "Motorboat"}, classifications: {name: "Sailboat"}).uniq
     #joins(:boats, :classifications).where(classifications: {name: "Motorboat"}, classifications: {name: "Sailboat"}).uniq
-    sailors.includes(boats: :classifications).where(classifications: {name: "Motorboat"})
-
+    #sailors.includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboaters.pluck(:id))
 
   end
 
   def self.non_sailors
     #includes(boats: :classifications).where.not(classifications: {name: "Sailboat"})
-    joins(:boats, :classifications).group(:classification_id).where.not(classifications: {name: "Sailboat"})
+    #joins(:boats, :classifications).group(:classification_id).where.not(classifications: {name: "Sailboat"})
+    where.not("id IN (?)", self.sailors.pluck(:id))
   end
 end
