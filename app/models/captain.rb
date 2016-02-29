c2fbc24e99ea9297abf2bcc822f225f280ed7af4
febbraiod <donedesign1@gmail.com>
2016-02-29 13:36:41 -0500

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,28 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+
+  def self.catamaran_operators
+    joins(boats: :classifications).where("classification_id == ? ", 3).uniq
+  end
+
+  def self.sailors
+    joins(boats: :classifications).where("classification_id == ? ", 2).uniq
+  end
+
+  def self.motordudes
+    joins(boats: :classifications).where("classification_id == ? ", 5).uniq
+  end
+
+  def self.talented_seamen
+    arr = self.sailors.pluck(:id) & self.motordudes.pluck(:id)
+    where(id: arr)
+  end
+
+  def self.non_sailors
+    #how about all captains not included in self.sailors
+    where.not(id: self.sailors.pluck(:id))
+  end
+
+
 end
