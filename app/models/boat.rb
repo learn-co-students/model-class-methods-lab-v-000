287950e717f141fb7e0c5e61367940976a9acd04
marcotsui2003 <marcotsui2003@yahoo.com>
2016-03-15 21:37:35 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,38 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+  	self.limit(5)
+  end
+
+  def self.dinghy
+  	self.where("length < 20" )
+  end
+
+  def self.ship
+  	self.where("length >= 20")
+  end
+
+  def self.last_three_alphabetically 
+  	self.order('name DESC').limit(3).order('name ASC')
+  end
+
+  def self.without_a_captain
+  	self.where('captain_id is NULL')
+  end
+
+  def self.sailboats
+  	#sql= <<-SQL
+  	#  SELECT boats.* FROM boats
+    #  INNER JOIN boat_classifications ON boats.id = classifications.id
+    #  INNER JOIN classifications ON boat_classifications.classification_id = classifications.id
+    #SQL
+  	self.joins(:classifications).where('classifications.name = "Sailboat"')
+  end
+
+  def self.with_three_classifications
+  	 Boat.joins(:classifications).group("boats.name").having("count(*) = 3")
+  end
+  
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,15 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+  	all
+  end
+
+  def self.longest
+  	boat_id = Boat.order("length DESC").limit(1).pluck(:id)
+  	Classification.joins(:boats).where("boats.id in (?)", boat_id)
+  end
+
+    
 end
