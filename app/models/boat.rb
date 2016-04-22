ef7bbf477652c32dece0c39769af8cb8759b2ac2
agdavid <antonio.g.david@gmail.com>
2016-04-22 17:05:46 -0400

    Done. Code model class methods using ActiveRecord relationships.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,40 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.all.limit(5)
+  end
+
+  def self.dinghy
+    self.where("length < ?", 20)
+  end
+
+  def self.ship
+    self.where("length >= ?", 20)
+  end
+
+  def self.last_three_alphabetically
+    self.order("name DESC").limit(3)
+  end
+
+  def self.without_a_captain
+    self.where("captain_id IS NULL")
+  end
+
+  def self.sailboats
+    #sailboat_id = Classification.find_by(name: "Sailboat").id
+    #self.joins(:boat_classifications).where("classification_id = ?", sailboat_id)
+    self.joins(:classifications).where("classifications.name = ?", "Sailboat")
+  end
+
+  def self.with_three_classifications
+    self.joins(:classifications).group("boat_id").having("COUNT(*) = ?", 3)
+  end
+
+  #helper method for Classification.longest
+  def self.longest
+    self.order("length").last
+  end
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    #use helper method from Boat class then call #classifications on that
+    Boat.longest.classifications
+  end
+
 end
