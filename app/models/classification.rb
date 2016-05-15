93ceaeb0a27f015adb73cf08185b7d5af4f581ab
katherinethai <kthai1618@gmail.com>
2016-05-15 18:46:06 -0400

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    Classification.all
+  end
+
+  def self.longest
+    max_length = Boat.all.pluck(:length).max
+    Boat.find_by(length: max_length).classifications
+  end
 end
