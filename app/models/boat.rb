ebbb86ec02eaa3a6d98a7dd125466ef087a7645e
Nathaniel Miller <dukeoflaser@gmail.com>
2016-06-02 04:12:41 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,46 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+  
+  
+  class << self
+    def first_five
+      limit 5
+    end
+    
+    def dinghy
+      where('length < ?', 20)
+    end
+    
+    def ship
+      where('length >= ?', 20)
+    end
+    
+    def last_three_alphabetically
+      order(name: :desc).limit 3
+    end
+    
+    def without_a_captain
+      where(captain_id: nil)
+    end
+    
+    def sailboats
+      #Return all Boat objects that have a classification. 
+      #Filter further to a classification with a name of Sailboat.
+      joins(:classifications).where(classifications: { name: 'Sailboat' })
+    end
+    
+    def with_three_classifications
+      #A boat object for every boat/classification record.
+      #Group records with identical boat ids into a single record.
+      #Of those groups, return the ones that have a classification_id count of 3.
+      joins(:classifications).group('boats.id').having('COUNT(classification_id) = 3')
+    end
+    
+    def longest
+      order(length: :desc).limit(1).first
+    end
+  end
+  
+  
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,29 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  
+  class << self
+    def catamaran_operators
+      joins(:boats => :classifications).where(:classifications => {name: 'Catamaran'})
+    end
+    
+    def sailors
+      joins(:boats => :classifications).where(:classifications => {name: 'Sailboat'}).group('captains.id')
+    end
+    
+    def uses_a_motor
+      joins(:boats => :classifications).where("classifications.name = ?", 'Motorboat').group('captains.id')
+    end
+    
+    def talented_seamen
+      where("id IN (?)", talented_ids)
+    end
+    
+    def non_sailors
+      where.not("name IN (?)", sailors.pluck(:name))
+    end
+    
+    def talented_ids
+      sailors.pluck(:id) & uses_a_motor.pluck(:id)
+    end
+  end
 end
