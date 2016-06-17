1ffab3de3b96d9a03542a534c3123a8ccb73b5cb
lawrend <douglaslawrence314@gmail.com>
2016-06-16 21:58:05 -0700

    done, finally

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,36 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.boat_class(classname)
+  	Boat.joins(:classifications).where( classifications: { :name => classname } )
+  end
+
+  def self.captain_id_extractor(boats)
+  	boats.map { |boat| boat.captain_id }
+  end
+  
+  def self.catamaran_operators
+  	@boats = captain_id_extractor(boat_class("Catamaran"))
+  	Captain.joins(:boats).distinct.where(boats: { captain_id: [@boats]})
+  end
+
+  def self.sailors
+  	@boats = captain_id_extractor(boat_class("Sailboat"))
+  	Captain.joins(:boats).distinct.where(boats: { captain_id: [@boats]})
+  end
+
+  def self.talented_seamen
+  	@motorboats = captain_id_extractor(boat_class("Motorboat"))
+  	@sailboats = captain_id_extractor(boat_class("Sailboat"))
+  	@boats = @motorboats & @sailboats
+  	Captain.joins(:boats).distinct.where(boats: { captain_id: [@boats]})
+  end
+
+  def self.non_sailors
+  	@all = captain_id_extractor(Boat.all)
+  	@sailboats = captain_id_extractor(boat_class("Sailboat"))
+  	@boats = @all - @sailboats
+  	Captain.joins(:boats).distinct.where(boats: { captain_id: [@boats]})
+  end
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+  	Classification.all 
+  end
+
+  def self.longest
+  	@boat = Boat.maximum("length")
+  	Classification.joins(:boats).where( boats: { length: @boat } )
+  end
+
 end
