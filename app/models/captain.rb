a046451b84213e774c420b3b595643391d3e4159
kaileighrose <kaileighrose@gmail.com>
2016-08-13 00:39:50 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,39 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats
+
+  def Captain::catamaran_operators
+    classification = Classification.where(name: "Catamaran")
+    classification[0].captains
+  end
+
+  def Captain::sailors
+    captains = Boat.sailboats.pluck(:captain_id)
+    Captain.where(id: captains)
+  end
+
+  def Captain::talented_seamen
+    sailcaptains = Captain.sailors.pluck(:id)
+    classification = Classification.where(name: "Motorboat")
+    motorcaptains = []
+    classification[0].captains.each { |c| motorcaptains << c.id}
+    allcaptains = []
+    sailcaptains.each do |c|
+      if motorcaptains.include?(c)
+        allcaptains << c
+      end
+    end
+    Captain.where(id: allcaptains)
+  end
+
+  def Captain::non_sailors
+    allcaptains = []
+    sailcaptains = Captain.sailors.pluck(:id)
+    Captain.all.each do |c|
+      if !sailcaptains.include?(c.id)
+        allcaptains << c.id
+      end
+    end
+    Captain.where(id: allcaptains)
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+  has_many :captains, through: :boats
+
+  def Classification::my_all
+    self.all
+  end
+
+  def Classification::longest
+    boat = Boat.order(:length).last
+    boat.classifications
+  end
 end
