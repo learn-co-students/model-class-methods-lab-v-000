3ba72cf2ea7cde95afa9ea7a50585702eb8f1273
Christian Whitesides <whitesidescr@gmail.com>
2016-08-17 14:08:14 -0600

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,27 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins(boats: [{boat_classifications: :classification}]).where('classifications.name = ?', 'Catamaran')
+
+    # SELECT captains.* FROM captains
+    #   INNER JOIN boats ON boats.captain_id = captains.id
+    #   INNER JOIN boat_classifications ON boat_classifications.boat_id = boats.id
+    #   INNER JOIN classifications ON classifications.id = boat_classifications.classification_id
+    #   WHERE (classifications.name = 'Catamaran')
+  end
+
+  def self.sailors
+    joins(boats: [{boat_classifications: :classification}]).where('classifications.name = ?', 'Sailboat').distinct
+
+    # SELECT DISTINCT captains.* FROM captains
+    #   INNER JOIN boats ON boats.captain_id = captains.id
+    #   INNER JOIN boat_classifications ON boat_classifications.boat_id = boats.id
+    #   INNER JOIN classifications ON classifications.id = boat_classifications.classification_id
+    #   WHERE (classifications.name = 'Sailboat')
+  end
+
+  def self.talented_seamen
+    
+  end
 end
