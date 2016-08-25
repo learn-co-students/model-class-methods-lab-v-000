1dc295ce830b53be308619cc87a266394dbb8eb0
Michael Chen <michael.chen.1120@gmail.com>
2016-08-25 02:41:54 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    self.includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+  end
+
+  def self.motorboaters
+    self.includes(boats: :classifications).where(classifications: {name: "Motorboat"}).distinct
+  end
+
+  def self.talented_seamen
+    self.where(id: self.sailors.pluck(:id) & self.motorboaters.pluck(:id))
+  end
+
+  def self.non_sailors
+    self.where.not(id: self.sailors.pluck(:id))
+  end
+
 end
