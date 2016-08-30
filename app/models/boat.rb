c3489141645236912dbd1bedd4deb5c3ca35d3f8
Joe <jsbenning4@gmail.com>
2016-08-30 10:14:49 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,31 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  
+  def self.first_five
+    Boat.limit(5) 
+  end
+
+  def self.dinghy
+    Boat.where("length < 20")
+  end
+
+  def self.ship
+    Boat.where("length > 20")
+  end
+
+  def self.last_three_alphabetically
+    Boat.order(:name => :desc).limit(3).all
+  end
+
+  def self.without_a_captain
+    Boat.where(captain_id: [false, nil])
+  end
+
+  def self.sailboats
+    Boat.joins(:boat_classifications => :classification).where(:classifications => {:name => "Sailboat"})
+  end
+
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,13 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+  end
+
+
+  def self.sailors
+    Captain.where(self.boat.classification.include?("catamaran"))
+  end
+
+
 end
