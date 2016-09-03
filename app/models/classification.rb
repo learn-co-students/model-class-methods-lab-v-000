b67f57682566ed8bd2713251db0e87b5b97c4198
jwolfe890 <jwolfe890@gmail.com>
2016-09-03 01:05:15 -0400

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,25 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+ def self.longest
+    Boat.longest.first.classifications
+  end
+
 end
+
+
+
+#  limit(3).order(name: :desc) 
+# end
+
+# def self.without_a_captain
+#   where(captain_id: nil)
+# end
+
+# def self.sailboats
+#   joins(:classifications).where('classifications.name' => "Sailboat")
