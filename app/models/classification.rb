4e1d1b638f232d175c537f851cdc631a262d7153
CodeByLine <yleventhal@ymail.com>
2016-09-04 19:38:00 -0400

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    # SELECT * FROM Classification
+    all
+  end
+
+  def self.longest
+    # joins(:boats).where('boats'.order(length: :desc)),  limit(1)
+    includes(:boats).where('boats.length = (SELECT MAX(length) from boats)')
+  end
 end
