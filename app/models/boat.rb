6c31b6bd9d9f53d6fd750901f13aa63068d2b038
peterberwind <peterberwind@gmail.com>
2016-09-06 20:40:37 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,33 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.order('created_at ASC').limit(5)
+  end
+
+  def self.dinghy
+    self.where(length: 0..20)
+  end
+
+  def self.ship
+    self.where("length > ?", 20)
+  end
+
+  def self.last_three_alphabetically
+    self.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    self.where(captain_id: nil)
+  end
+
+  def self.sailboats
+    includes(:classifications).where("classifications.name" => "Sailboat")
+  end
+
+  def self.with_three_classifications
+    includes(:classifications).group("boats.name").having("count(boat_id) = 3")
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,27 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    # self.joins(:classifications).where(classifications: {name: "Catamaran"})
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def self.motorboaters
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+  end
+
+  def self.non_sailors
+    # look in Captions Table, run .sailros method, return ID's that ARE NOT sailors
+    where.not("id IN(?)", self.sailors.pluck(:id))
+  end
+
+  def self.talented_seamen
+    # look in Captons Table, run sailors method, return ID's that ARE sailors
+    # Take those results and do the same for Moterboaters
+    where("id IN(?)", self.sailors.select(:id) & self.motorboaters.select(:id))
+  end
 end
