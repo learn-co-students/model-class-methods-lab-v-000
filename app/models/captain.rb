2189e1a39b919cdda74cc7105a28fd43f06209f3
ed-karabinus <ejkarabin@gmail.com>
2016-09-30 04:15:26 -0500

    Not yet finished; 3 tests still failing and 3 class methods not yet fleshed out.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,20 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, :through => :boats
+
+  def self.catamaran_operators
+    joins(:classifications).where("classifications.name =?", "Catamaran").uniq
+  end
+
+  def self.sailors
+    joins(:classifications).where("classifications.name =?", "Sailboat").uniq
+  end
+
+  def self.talented_seamen
+
+  end
+
+  def self.non_sailors
+
+  end
 end
