ca31fed43c600aaab3e3ff30af4ea603f717056c
Traci7822 <Tracir@gmail.com>
2016-10-12 13:30:48 -0600

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,39 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    select(:name).limit(5)
+  end
+
+  def self.dinghy
+    select(:name).where("length <?", 20)
+  end
+
+  def self.ship
+    select(:name).where("length >=?", 20)
+  end
+
+  def self.last_three_alphabetically
+    select(:name).order(:name => :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    select(:name).where("captain_id" => nil)
+  end
+
+  def self.sailboats
+    includes(:classifications).where('classification_id = ?', find_classification("Sailboat").id)
+  end
+
+  def self.with_three_classifications
+    joins(:classifications).group('boats.id').having("COUNT(*) = 3")
+  end
+
+  private
+
+  def self.find_classification(type)
+    Classification.find_by(name: type)
+  end
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,20 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    select(:classifications)
+  end
+
+  def self.longest
+    select(:boats).where("id IN (?)", longest_boat_classifications)
+  end
+
+  private
+
+  def self.longest_boat_classifications
+    Boat.all.sort{|x, y| y.length <=> x.length}.first.classifications.map do |classification|
+      classification.id
+    end
+  end
 end
