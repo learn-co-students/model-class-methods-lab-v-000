d3802962a6e5893f47777972ead00aa35f8c2819
Lenn Hypolite <lenntriniman@hotmail.com>
2016-10-13 03:42:39 +0000

    Initial commit

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,17 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+
+  def self.first_five
+    self.limit(5)
+  end
+
+  def self.dinghy
+    self.where(:length => [0..19])
+  end
+
+  def self.dinghy
+    self.where(:length => [0..19])
+  end
 end
