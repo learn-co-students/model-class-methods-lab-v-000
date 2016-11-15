aa1b4121a1077346763d6fdd7565ae8b60796e5e
Clare Horton <clare.e.horton@gmail.com>
2016-11-15 01:31:25 -0600

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,40 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five # returns the first five Boats
+    self.limit(5)
+  end
+
+  def self.dinghy # returns boats shorter than 20 feet
+    self.where('boats.length < ?', 20)
+  end
+
+  def self.ship # returns boats 20 feet or longer
+    self.where('boats.length > ?', 20)
+  end
+
+  def self.last_three_alphabetically # returns last three boats in alphabetical order
+    self.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain # returns boats without a captain
+    self.where(captain: nil)
+  end
+
+  def self.sailboats # returns all boats that are sailboats
+    Classification.find_by(name: "Sailboat").boats
+  end
+
+  def self.with_three_classifications # returns boats with three classifications
+    self.joins(:classifications).group("boat_id").having("count(*) = ?", 3)
+  end
+
+  def self.longest
+    self.order(length: :desc).first
+  end
+
+  def self.sailboats
+    self.joins(:classifications).where('classifications.name = ?', 'Sailboat')
+  end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,25 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :boat_classifications, through: :boats
+  has_many :classifications, through: :boat_classifications
+
+  def self.catamaran_operators # returns all captains of catamarans
+    self.joins(:classifications).where('classifications.name = ?', 'Catamaran')
+  end
+
+  def self.sailors # returns captains with sailboats
+    self.joins(:classifications).where('classifications.name = ?', 'Sailboat').uniq
+  end
+
+  def self.motorboaters
+    self.joins(:classifications).where('classifications.name = ?', 'Motorboat').uniq
+  end
+
+  def self.talented_seamen # returns captains of motorboats and sailboats
+    self.where('id IN (?)', self.sailors.pluck(:id) & self.motorboaters.pluck(:id))
+  end
+
+  def self.non_sailors # returns people who are not captains of sailboats
+    self.where.not('id IN (?)', self.sailors.pluck(:id))
+  end
 end
