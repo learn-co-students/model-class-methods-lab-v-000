dbbe2c24672c4884e9b5d75f7d370c15ddbda21e
Will Lynch <will.lynch91@gmail.com>
2016-11-16 18:33:39 -0500

    working on captain. boat is done

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,33 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.limit(5)
+  end
+
+  def self.dinghy
+    self.where("length < ?", 20)
+  end
+
+  def self.ship
+    self.where("length >= ?", 20)
+  end
+
+  def self.last_three_alphabetically
+    self.order('name desc').limit(3)
+  end
+
+  def self.without_a_captain
+    self.where("captain_id IS NULL")
+  end
+
+  def self.sailboats
+    self.includes(:classifications).where('classifications.name = ?', 'Sailboat')
+  end
+
+  def self.with_three_classifications
+    self.includes(:boat_classifications).having('COUNT(classification_id) = ?', 3).group('boat_id')
+  end
+
 end
