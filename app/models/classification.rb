19883daa759572ca450403bdc3341763ac9a266f
hilaryml <hilyttle@gmail.com>
2016-12-01 13:36:07 -0500

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    all
+  end
+
+  def self.longest
+    boat = Boat.longest
+    includes(:boats).where(boats: {id: boat.id})
+  end
 end
