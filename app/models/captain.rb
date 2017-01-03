d40f1872b77927763d9b56dea9d9957d5815b13a
Jennifer Pazos <jmp521@gmail.com>
2017-01-03 23:20:15 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -6,12 +6,12 @@ class Captain < ActiveRecord::Base
     includes(boats: :classifications).where(classifications: {name: 'Catamaran'})
 
   end
-  def self.sailors 
-    includes(boats: :classifications).where(classifications: {name: 'Sailboat'})
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: 'Sailboat'}).uniq
 
   end
   def self.talented_seamen
-
+    includes(boats: :classifications).where(classifications: {name: 'Motorboats', name: 'Sailboat'})
   end
   def self.non_sailors
 
