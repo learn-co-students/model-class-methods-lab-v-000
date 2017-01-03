301555e27a74342b034f776a237ab71b6bfff48d
Jennifer Pazos <jmp521@gmail.com>
2017-01-03 22:14:16 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,18 @@
+require 'pry'
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+      @test = includes(boats: :classifications).where(classifications: {name: 'Catamaran'})
+    binding.pry
+  end
+  def self.sailors
+
+  end
+  def self.talented_seamen
+
+  end
+  def self.non_sailors
+
+  end
 end
