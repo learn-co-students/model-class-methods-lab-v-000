5729a95968a707d9356d0d73d893d08e6ddbbffe
Bill <storywr@gmail.com>
2017-01-17 15:33:04 -0500

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins(boats: :classifications).where('classifications.name == ?', 'Catamaran').uniq
+  end
+
+  def self.sailors
+    joins(boats: :classifications).where('classifications.name == ?', 'Sailboat').uniq
+  end
+
+  def self.talented_seamen
+    motor_guys = joins(boats: :classifications).where('classifications.name == ?', 'Motorboat').uniq
+    sailboat_guys = joins(boats: :classifications).where('classifications.name == ?', 'Sailboat').uniq
+    motor_guys & sailboat_guys
+  end
+
+  def self.non_sailors
+    no_sailboat_guys = joins(boats: :classifications).where.not('classifications.name == ?', 'Sailboat').uniq
+    sailboat_guys = joins(boats: :classifications).where('classifications.name == ?', 'Sailboat').uniq
+    no_sailboat_guys - sailboat_guys
+  end
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    all
+  end
+
+  def self.longest
+    joins(:boats).limit(1).order('length desc')
+  end
+
 end
