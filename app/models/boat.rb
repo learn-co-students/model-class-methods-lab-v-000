6fa6113b8327dd6b44267fab9ed37e610e3ee0b5
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-01-17 23:49:26 -0500

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -7,6 +7,7 @@ class Boat < ActiveRecord::Base
     limit(5)
   end
 
+
   def self.dinghy
     where("length < ?", 20)
   end
@@ -18,6 +19,19 @@ class Boat < ActiveRecord::Base
   def self.last_three_alphabetically
     order(name: :desc).limit(3)
   end
+
+  def self.without_a_captain
+    where(captain_id: nil )
+  end
+
+  def self.sailboats
+    joins("INNER JOIN boat_classifications ON boat_classifications.boat_id = boats.id INNER JOIN classifications ON  classifications.id = boat_classifications.classification_id AND classifications.name = 'Sailboat'")
+  end
+
+  def self.with_three_classifications
+    joins(:boat_classifications).having('COUNT(boat_classifications.classification_id) = 3').group('boat_classifications.boat_id')
+  end
+
 
 
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,15 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins(boats: [boat_classifications: :classification]).where('classifications.name = "Catamaran"')
+  end
+
+  def self.sailors
+    joins(boats: [boat_classifications: :classification]).where('classifications.name = "Sailboat"').distinct
+  end
+
+  def self.talented_seamen
+    joins(boats: [boat_classifications: :classification]).where('classifications.name = "Sailboat" AND classifications.name = "Motorboat"')
+  end
 end
