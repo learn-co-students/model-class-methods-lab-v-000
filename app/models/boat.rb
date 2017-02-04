9fb995c580f574949da185afce63aa05cdf09a5d
WilliamMena <williammena1991@gmail.com>
2017-02-04 17:30:11 -0500

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,11 @@
+require 'pry'
 class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    all[0..4]
+    # Boat.first(5).pluck(:name)
+  end
 end
