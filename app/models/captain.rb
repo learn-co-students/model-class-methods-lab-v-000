ceea3344eec66b991da1942e466560a46c1e5875
Adam Lucia <lucia.adam@gmail.com>
2017-02-10 23:23:30 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins(boats: [:classifications]).where("classifications.name = ?", "Catamaran")
+  end
+
+  def self.sailors
+    joins(boats: [:classifications]).where("classifications.name = ?", "Sailboat").distinct
+  end
+
+  def self.talented_seamen
+    mb_ids = self.joins(boats: [:classifications]).where("classifications.name = ?", "Motorboat").pluck(:id)
+
+    self.sailors.where(id: mb_ids)
+  end
+
+  def self.non_sailors
+    non_sailor_ids = self.all.pluck(:id) - self.sailors.pluck(:id)
+
+    where(id: non_sailor_ids)
+  end
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,18 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    long_boat = 0
+    Boat.all.each do |boat|
+      if boat.length > long_boat
+        long_boat = boat.length
+      end
+    end
+    Boat.find_by(length: long_boat).classifications
+  end
 end
