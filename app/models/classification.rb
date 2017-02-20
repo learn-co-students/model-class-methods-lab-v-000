4739ed8d4da6502826bca02451474a4a9cb82609
Keegan Leitz <kjleitz@gmail.com>
2017-02-20 12:56:09 -0500

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,16 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  class << self
+
+    def my_all
+      all
+    end
+
+    def longest
+      Boat.longest.classifications
+    end
+
+  end
 end
