aff20475a172fe56290f0971da87436fd74dc8f8
github username <github email address>
2017-03-07 16:23:59 -0500

    completed

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    all
+  end
+
+  def self.longest
+    longboat = Boat.order(length: :desc).limit(1)
+    joins(:boats).where('boat_id = ?', longboat.ids)
+  end
 end
