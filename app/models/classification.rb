d4a018c1de128677ae99310d98f48baa88391eb0
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-03-11 13:54:07 +0000

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    all
+  end
+
+  def self.longest
+    boat = Boat.order(length: :desc).limit(1)
+    Classification.joins(:boats).where('boat_id = ?', boat.ids)
+  end
 end
