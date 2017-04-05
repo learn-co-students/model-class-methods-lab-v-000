1cfebd0892a6150b2be6591743238f9c401cc56f
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-04-05 11:48:05 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,28 @@
 class Captain < ActiveRecord::Base
-  has_many :boats
+    has_many :boats
+
+    def self.catamaran_operators
+        
+        Captain.select("captains.*").joins(boats: :classifications).where("classifications.name = ?","Catamaran")    
+    end
+
+    def self.sailors
+        Captain.select("captains.*").distinct.joins(boats: :classifications).where("classifications.name = ?","Sailboat")    
+    end
+
+    def self.motorboat_captains
+       Captain.select("captains.*").joins(boats: :classifications).where("classifications.name = ?","Motorboat") 
+    end
+    def self.talented_seamen
+        #Need to find the intersection of:
+#        Captain.select("captains.*").joins(boats: :classifications).where("classifications.name = ?","Sailboat") 
+#        Captain.select("captains.*").joins(boats: :classifications).where("classifications.name = ?","Motorboat")
+        where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_captains.pluck(:id))
+        
+    end
+
+    def self.non_sailors
+        where.not("id IN (?)", self.sailors.pluck(:id))
+    end
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
-  has_many :boat_classifications
-  has_many :boats, through: :boat_classifications
+    has_many :boat_classifications
+    has_many :boats, through: :boat_classifications
+
+    def self.my_all
+        Classification.all
+    end
+
+    def self.longest
+#        max_length = Boat.order("length DESC").first
+#        Classification.joins(:boats).where("length = ?", max_length.length)
+        Boat.order(length: :desc).first.classifications
+    end
 end
