8d0fcd634fe008462cb8036c8c030a4558e04cc7
github username <jilustrisimo@users.noreply.github.com>
2017-04-05 10:35:46 -0400

    Abstracted Classification.longest

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -7,7 +7,8 @@ class Classification < ActiveRecord::Base
   end
 
   def self.longest
-    longboat = Boat.order(length: :desc).limit(1)
-    joins(:boats).where('boat_id = ?', longboat.ids)
+    # longboat = Boat.order(length: :desc).limit(1)
+    # joins(:boats).where('boat_id = ?', longboat.ids)
+    Boat.order(length: :desc).first.classifications
   end
 end
