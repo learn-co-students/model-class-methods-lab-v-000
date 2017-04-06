0fd78e01ffe6fb5ba249d4f12f3de0cbcdeb6d7e
John Pfingst <jpfingst@outlook.com>
2017-04-06 04:18:11 +0000

    activerecord practice

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,20 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.joins(boats: :classifications).where(classifications: {name: "Catamaran"}).distinct
+  end
+
+  def self.non_sailors
+    self.where("id not in (?)", self.sailors.pluck(:id))
+  end
+
+  def self.sailors
+    self.joins(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+  end
+
+  def self.talented_seamen
+    self.joins(boats: :classifications).where(classifications: {name: ['Motorboat', 'Sailboat']}).distinct
+  end
+
 end
