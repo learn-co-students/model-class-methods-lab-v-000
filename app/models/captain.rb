37de59a69a7edb852fc51b4509c43fb90a4ae00a
John Schlesinger <jschles1@ramapo.edu>
2017-04-19 01:26:33 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,23 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins(boats: :classifications).where(classifications: {name: 'Catamaran'})
+  end
+
+  def self.sailors
+    joins(boats: :classifications).where(classifications: {name: 'Sailboat'}).uniq
+  end
+
+  def self.motorboats
+    joins(boats: :classifications).where(classifications: {name: 'Motorboat'}).uniq
+  end
+
+  def self.talented_seamen
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboats.pluck(:id))
+  end
+
+  def self.non_sailors
+    where.not("id IN (?)", self.sailors.pluck(:id))
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,12 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    where("name != ''")
+  end
+
+  def self.longest
+    joins(:boats).where("length == (?)", Boat.maximum("length"))
+  end
 end
