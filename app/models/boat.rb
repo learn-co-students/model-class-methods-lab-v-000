b6ab72f9d24bb3b0b35865e73376e0b0d217aac1
DanL12186 <danlrcn@hotmail.com>
2017-05-03 13:59:40 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,33 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    Boat.first(5) #limit(5)
+  end
+
+  def self.dinghy
+    Boat.select {|boat| boat.length < 20} #where("length <= 20")
+  end
+
+  def self.last_three_alphabetically
+    Boat.all.sort_by {|boat| boat.name}.last(3).reverse #test is backwards. Boat.order(:name).last(3).reverse; ##Boat.order(name: :desc).limit(3)##
+  end
+
+  def self.ship
+    Boat.select {|boat| boat.length > 20} #Boat.where(["length > ?", 20])
+  end
+
+  def self.without_a_captain
+    Boat.select {|boat| boat.captain.nil?} #Boat.where(captain: nil)
+  end
+
+  def self.sailboats
+    includes(:classifications).where(classifications: {name: "Sailboat"})
+  end
+
+  def self.with_three_classifications
+    select {|boat| boat.classifications.length == 3}
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,25 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    # Boat.joins(:classifications).includes(:captain).where(classifications: {name: "Catamaran"}).map {|boat| boat.captain}
+    Captain.joins(boats: :classifications).where(classifications: {name: "Catamaran"}).uniq
+  end
+
+  def self.sailors
+    Captain.joins(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def self.motorboaters
+    Captain.joins(boats: :classifications).where(classifications: {name: "Motorboat"}).uniq
+  end
+
+  def self.talented_seamen
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboaters.pluck(:id))
+  end
+
+  def self.non_sailors
+    where.not("id IN (?)", self.sailors.pluck(:id))
+    # self.select {|captain| !captain.name.match(/(Captain)/)}
+  end
 end
