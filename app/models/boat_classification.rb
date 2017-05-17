f6fc91f927412b9447fdae3c86c1f8a76c36fe48
anytimesoon <damien@delartigue.co.uk>
2017-05-18 00:21:06 +0100

    Done.

--- app/models/boat_classification.rb
+++ app/models/boat_classification.rb
@@ -1,4 +1,9 @@
 class BoatClassification < ActiveRecord::Base
   belongs_to :boat
   belongs_to :classification
+
+  def self.order_by_boat_id
+  	self.order(:boat_id)
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,34 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+  	self.classification_list('Catamaran').uniq
+  end
+
+  def self.sailors
+  	self.classification_list('Sailboat').uniq
+  end
+
+  def self.motorboaters
+  	self.classification_list('Motorboat').uniq
+  end
+
+  def self.talented_seamen
+    self.where("name in (?)", self.sailors.pluck(:name) & self.motorboaters.pluck(:name))
+  end
+
+  def self.non_sailors
+  	self.where("name not in (?)", self.sailors.pluck(:name))
+  end
+
+  private
+
+  	def self.classification_list(boat_class)
+  		self.all.includes(boats: :classifications).where('classifications.name = ?', boat_class)
+  	end
+
+	  def self.classification_join
+	  	'INNER JOIN boats ON boats.captain_id = captains.id INNER JOIN boat_classifications ON boat_classifications.boat_id = boats.id INNER JOIN classifications ON boat_classifications.classification_id = classifications.id' 
+	  end
+
 end
