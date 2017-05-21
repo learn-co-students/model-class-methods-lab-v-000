06bc1263b010eaa0ff4357edb98d3cc927f743f5
Christina L. Cole <christinalcole@gmail.com>
2017-05-21 19:15:30 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,32 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    all.limit(5)
+  end
+
+  def self.dinghy
+    where("length < ?", 20)
+  end
+
+  def self.ship
+    where("length >= ?", 20)
+  end
+
+  def self.last_three_alphabetically
+    all.order(:name).reverse_order.limit(3)
+  end
+
+  def self.without_a_captain
+    where("captain_id is null") # can't use captain_id = nil because db uses three-value logic; null is neither true not false, can't be stated as 'equal to'
+  end
+
+  def self.sailboats
+    joins(:classifications).where("classifications.name = ?", "Sailboat")
+  end
+
+  def self.with_three_classifications
+    joins(:classifications).group(:boat_id).having("COUNT(*) = 3")
+  end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,25 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins(boats: [:classifications]).where("classifications.name = ?", "Catamaran").references(:classifications)
+  end
+
+  def self.sailors
+    includes(boats: [:classifications]).where("classifications.name = ?", "Sailboat").references(:classifications).distinct
+  end
+
+  def self.motorboaters
+    includes(boats: [:classifications]).where("classifications.name =?", "Motorboat").references(:classifications).distinct
+  end
+
+  def self.talented_seamen
+    # Topic.find(:all, :conditions => { :forum_id => @forums.map(&:id) })
+
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboaters.pluck(:id))
+  end
+
+  def self.non_sailors
+    where.not("id IN (?)", self.sailors.pluck(:id))
+  end
 end
