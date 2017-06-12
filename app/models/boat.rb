a5b3551e88f5da7f7ba452d611f35bb2fc213caf
Menachem Hornbacher <me@mendel.tech>
2017-06-12 01:23:00 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,36 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.limit(5)
+  end
+
+  def self.dinghy
+    self.where("length < 20")
+  end
+
+  def self.ship
+    self.where("length > 20")
+  end
+
+  def self.last_three_alphabetically
+    self.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    self.where("captain_id is null")
+  end
+
+  def self.sailboats
+    # sailboat_classification = Classification.find_by(name: "Sailboat")
+    # self.all.select{|boat| boat.classifications.include?(sailboat_classification)}
+    #More efficient
+    # self.joins("join boat_classifications on boats.id = boat_classifications.boat_id join classifications on boat_classifications.classification_id = classifications.id").where("classifications.name = 'Sailboat'")
+    self.joins(:boat_classifications).joins(:classifications).where("classifications.name = 'Sailboat'").distinct
+  end
+
+  def self.with_three_classifications
+     self.joins(:boat_classifications).group('boats.id').having('count(classification_id) = 3')
+  end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,32 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    # Desired SQL
+    # select 'captains'.'*' from captains join boats on boats.captain_id = captains.id
+    # join boat_classifications on boat_classifications.boat_id = boats.id
+    # join classifications on boat_classifications.classification_id = classifications.id
+    # where classifications.name = 'Catamrans'
+    self.joins("join boats on boats.captain_id = captains.id").joins("join boat_classifications on boat_classifications.boat_id = boats.id").joins("join classifications on classifications.id = boat_classifications.classification_id").where("classifications.name = 'Catamaran'")
+  end
+
+  def self.sailors
+    # Wow, that is weird stntax, I'd better get into it
+    self.joins(boats: [boat_classifications: :classification]).where("classifications.name = 'Sailboat'").distinct
+  end
+
+  def self.talented_seamen
+    motor_ids = self.joins(boats: [boat_classifications: :classification]).where("classifications.name = 'Motorboat'").pluck(:id)
+
+    sailor_ids = self.sailors.pluck(:id)
+
+    self.where("id IN (#{motor_ids.join(", ")}) AND id IN (#{sailor_ids.join(", ")})")
+
+  end
+
+  def self.non_sailors
+    sailor_ids = self.sailors.pluck(:id)
+    self.where("id not in (#{sailor_ids.join(", ")})")
+  end
+
 end
