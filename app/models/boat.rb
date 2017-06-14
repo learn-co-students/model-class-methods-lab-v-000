d96b732cc4357730fa5441cab8c8cfac38535a51
Andrew <ajoeford@gmail.com>
2017-06-13 22:26:13 -0700

    working on joins query

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,29 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    Boat.limit(5)
+  end
+
+  def self.dinghy
+    Boat.where("length < 20")
+  end
+
+  def self.ship
+    Boat.where("length >= 20")
+  end
+
+  def self.last_three_alphabetically
+    Boat.order(:name).reverse_order.limit(3)
+  end
+
+  def self.without_a_captain
+    Boat.where("captain_id is null")
+  end
+
+  def self.sailboats
+    
+  end
+
 end
