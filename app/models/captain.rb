e08f0bfa74f79dce34c0ad672eb34bed03feb98c
Erika Hughes <ehughes@knights.ucf.edu>
2017-06-20 02:25:44 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,16 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats
+
+  def self.catamaran_operators
+    joins(:classifications).where(:classifications => {:name => "Catamaran"})
+  end
+
+  def self.sailors
+    joins(:classifications).where(:classifications => {:name => "Sailboat"}).uniq
+  end
+
+  def self.talented_seamen
+    joins(:classifications).where("name = ? AND name = ?", "Sailboat", "Motorboat")
+  end
 end
