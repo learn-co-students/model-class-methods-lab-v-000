d889595f747ac3e60a71fe1150a760e970a91b34
AJ <ajoeford@gmail.com>
2017-06-21 08:58:02 -0700

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -28,7 +28,7 @@ class Boat < ActiveRecord::Base
   end
 
   def self.with_three_classifications
-    Boat.joins(:classifications).group(:boat_id).having("Count() = 3")
+    Boat.joins(:classifications).group(:boat_id).having("Count(boat_id) = 3")
   end
 
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,6 +2,23 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    Captain.joins(:boats).where(boats.classifications: {name: "Catamaran"})
+    Captain.includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    Captain.includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def self.motorists
+    Captain.includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+  end
+
+  def self.talented_seamen
+    Captain.where('id IN (?)', self.sailors.pluck(:id) &
+    self.motorists.pluck(:id))
+  end
+
+  def self.non_sailors
+    Captain.where.not('id IN (?)', self.sailors.pluck(:id))
   end
 end
