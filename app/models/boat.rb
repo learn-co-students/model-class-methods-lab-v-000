8c1f0e2607b2ddf1e062465d15e12a241a371827
Pia Kim <pkim2320@gmail.com>
2017-07-23 18:03:08 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,47 @@
 class Boat < ActiveRecord::Base
-  belongs_to  :captain
-  has_many    :boat_classifications
-  has_many    :classifications, through: :boat_classifications
+
+    belongs_to  :captain
+    has_many    :boat_classifications
+    has_many    :classifications, through: :boat_classifications
+
+    def self.first_five
+      limit(5)
+    end
+
+    def self.dinghy
+      where("length < 20")
+    end
+
+    def self.ship
+      where("length >= 20")
+    end
+
+    def self.last_three_alphabetically
+      order(name: :desc).limit(3)
+    end
+
+    def self.without_a_captain
+      where(captain_id: nil)
+    end
+
+    def self.sailboats
+      includes(:classifications).where(classifications: {name: "Sailboat"})
+    end
+
+
+  # In the case of a belongs_to relationship, an association key can be used to specify the model if an Active Record object is used as the value. This method works with polymorphic relationships as well.
+  #
+  # Article.where(author: author)
+  # Author.joins(:articles).where(articles: { author: author })
+  #so, boats has_many classifications so joins(:classifications) ?
+    def self.with_three_classifications
+
+     joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+    end
+
+    # for use in classification.rb
+    def self.longest
+      order("length DESC").first
+    end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,25 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+def self.catamaran_operators
+  includes(boats: :classifications).where(classifications: {name: 'Catamaran'})
+end
+
+def self.sailors
+  includes(boats: :classifications).where(classifications: {name: 'Sailboat'}).uniq
+end
+
+def self.talented_seamen
+  where("id IN (?)", self.sailors.pluck(:id) & self.motorboats.pluck(:id))
+  # pluck can be used to query single or multiple columns from the underlying table of a model. It accepts a list of column names as argument and returns an array of values of the specified columns with the corresponding data type.
+end
+
+def self.motorboats
+  includes(boats: :classifications).where(classifications: {name: 'Motorboat'})
+end
+
+def self.non_sailors
+  where.not("id IN (?)", self.sailors.pluck(:id))
+end
+
 end
