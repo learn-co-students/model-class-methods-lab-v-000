9525ffb0d7b22580fd12fcfc4135c89dd0ae9c96
Will-Sauer <will@heueso.com>
2017-07-27 15:18:02 -0700

    finished

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.distinct
+  end
+
+  def self.longest
+    Classification.joins(:boats).merge(Boat.order(length: :desc)).limit(2)
+  end
+  
 end
