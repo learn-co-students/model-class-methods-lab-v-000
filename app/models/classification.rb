9be7394a059abccb70755a3e60db8baa1d579593
sarjumulmi <mulmi.sarju@gmail.com>
2017-07-29 23:36:40 -0400

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,12 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    all
+  end
+
+  def self.longest
+    where('Id IN (?)', Boat.order('length DESC').first.classifications.pluck(:id))
+  end
 end
