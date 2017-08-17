f7fdeb8e0a0dddb24a12aaeb8f4a1730a59ae731
jlcobos <luiscobosj@gmail.com>
2017-08-17 16:26:13 -0700

    Done.

--- app/models/boat_classification.rb
+++ app/models/boat_classification.rb
@@ -1,4 +1,27 @@
 class BoatClassification < ActiveRecord::Base
   belongs_to :boat
   belongs_to :classification
+
+  def self.sailboat
+    where(classification_id: Classification.sailboat).collect do |boat_classification|
+      boat_classification.boat_id
+    end
+  end
+
+  def self.motorboat
+    where(classification_id: Classification.motorboat).collect do |boat_classification|
+      boat_classification.boat_id
+    end
+  end
+
+  def self.catamaran
+    where(classification_id: Classification.catamaran).collect do |boat_classification|
+      boat_classification.boat_id
+    end
+  end
+
+  def self.boat_classification_count
+    i = select(:boat_id).group(:boat_id).having("count(boat_id) = 3")
+    i.collect{|c|c.boat_id}
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,25 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.sailboat
+    self.find_by_name("Sailboat").id
+  end
+
+  def self.catamaran
+    self.find_by_name("Catamaran").id
+  end
+
+  def self.motorboat
+    self.find_by_name("Motorboat").id
+  end
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    Boat.order(length: :desc).limit(1).first.classifications
+  end
+
 end
