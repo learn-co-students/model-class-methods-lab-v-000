da9cb6e69637750d383c575700d47aaa506e3f4d
Spencer Evans <spencevans@gmail.com>
2017-08-18 14:52:37 +0000

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,17 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    # finds the id of the longest boat
+    longest_boat_id = Boat.order(length: :desc).first.id
+    # joins classifications with boats and filters only those
+    # classifications which match the boat_id of the longest boat
+    self.joins(boat_classifications: :boat).where("boats.id = ?", longest_boat_id)
+  end
+
 end
