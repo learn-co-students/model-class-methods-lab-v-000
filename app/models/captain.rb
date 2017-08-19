1a5e602feca94e0dd80031dd169fe8b998e8369b
blinded93 <blinded93@gmail.com>
2017-08-19 14:42:12 -0700

    passed tests

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.joins(boats: [:classifications])
+        .where({classifications: {name: "Catamaran"} })
+  end
+
+  def self.sailors
+    self.joins(boats: [:classifications])
+        .where({classifications: {name: "Sailboat"} })
+        .distinct
+  end
+
+  def self.talented_seamen
+    seamen = self.sailors.pluck(:id) & self.joins(boats: [:classifications]).where(classifications: {name: "Motorboat"}).pluck(:id)
+    self.where(id: seamen)
+  end
+
+  def self.non_sailors
+    sailors = self.sailors.pluck(:id)
+    self.where.not(id: sailors)
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    longest_boat = Boat.order(length: :desc).limit(1).pluck(:id)
+    self.joins(:boats).where(boats: {id: longest_boat})
+  end
 end
