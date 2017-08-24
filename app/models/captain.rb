219ce77edda511976a31a3fd918ef149047ac7e4
Shannon McCormack <shannonkmc@hotmail.com>
2017-08-24 20:44:26 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,7 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
 end
