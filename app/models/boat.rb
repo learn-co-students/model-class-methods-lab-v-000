f7319b134745b03dd8b66ec617b51292747c42d9
skmcloughlin <skmcloughlin@gmail.com>
2017-09-05 10:39:17 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,19 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def table
+    Boat.arel_table
+  end
+
+  boats = Arel::Table.new(:boats)
+
+  def self.first_five
+    Boat.where(Boat.arel_table[:id].lteq(5))
+  end
+
+  def self.dinghy
+    Boat.where(Boat.arel_table[:length].lteq(20))
+  end
+
 end
