0535f3ef22c8229d593ac1ee8d2c5463bf7b5761
Terry Blue <terryblue@comcast.net>
2017-09-17 03:28:15 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,21 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    firstFive = self.limit(5)
+  end
+
+  def self.dinghy
+    dinghy = self.where('length < 20')
+  end
+
+  def self.ship
+    ship = self.where('length > 19')
+  end
+
+  def self.last_three_alphabetically
+    lastThreeAlphabetically = self.order('name desc').limit(3)
+  end
+
 end
