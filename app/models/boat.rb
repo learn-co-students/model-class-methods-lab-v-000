47d467c791d5e7beef385c2f615d7a03ac0e728c
Terry Blue <terryblue@comcast.net>
2017-09-17 05:18:12 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,19 +4,31 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    firstFive = self.limit(5)
+    self.limit(5)
   end
 
   def self.dinghy
-    dinghy = self.where('length < 20')
+    self.where('length < 20')
   end
 
   def self.ship
-    ship = self.where('length > 19')
+    self.where('length > 19')
   end
 
   def self.last_three_alphabetically
-    lastThreeAlphabetically = self.order('name desc').limit(3)
+    self.order('name desc').limit(3)
+  end
+
+  def self.without_a_captain
+    self.where(captain_id: nil)
+  end
+
+  def self.sailboats
+    self.joins(:classifications).where(classifications: {name: 'Catamaran'})
+  end
+
+  def self.with_three_classifications
+    BoatClassification.group(:boat_id).having('count(boat_id) = ?', 3).joins(:boat)
   end
 
 end
