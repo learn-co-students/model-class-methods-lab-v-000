f0c417badcae6704fe3ccea8fc5819f295f384df
Tyler <tbuchhei@alumni.nd.edu>
2017-09-21 11:14:31 -0500

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,21 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins(boats: [:boat_classifications, :classifications]).where("classifications.name = ? ", "Catamaran").distinct
+  end
+
+  def self.sailors
+    joins(boats: [:boat_classifications, :classifications]).where("classifications.name = ? ", "Sailboat").distinct
+  end
+
+  def self.talented_seamen
+    joins(boats: [:boat_classifications, :classifications]).where("classifications.name = ? OR classifications.name = ?", "Sailboat", "Motorboat").group("classifications.name").order(:name)
+  end
+
+  def self.non_sailors
+    sailors = joins(boats: [:boat_classifications, :classifications]).select(:id).where("classifications.name = ?", "Sailboat")
+    where("id NOT IN (?)", sailors.pluck(:id))
+  end
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    all
+  end
+
+  def self.longest
+    max = Boat.select("*, Max(boats.length)")
+    joins(:boat_classifications, :boats).where("boats.length = ?", max.first.length).group("classifications.name").order("classifications.name DESC")
+  end
+
 end
