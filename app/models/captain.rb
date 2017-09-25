e945d619b95a3a4df012e47dcffb6550eb6ef770
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-09-25 18:36:37 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,19 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.includes(boats: :classifications).where(:classifications => {:name => "Catamaran"})
+  end
+
+  def self.sailors
+    self.includes(boats: :classifications).where(:classifications => {:name => "Sailboat"}).distinct
+  end
+
+  def self.talented_seamen
+
+    motorboat_operators = self.includes(boats: :classifications).where(:classifications => {:name => "Motorboat"}).distinct
+
+    all = motorboat_operators.pluck(:id) & self.sailors.pluck(:id)
+    self.where(id: all)
+  end
 end
