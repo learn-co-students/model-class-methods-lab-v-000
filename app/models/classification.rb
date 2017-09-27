b07f4906cab2c3317b6a1771af170fce62ae1d91
Soumya Veer <veer.soumya@gmail.com>
2017-09-27 19:31:06 +0000

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all.uniq
+  end
+
+  def self.longest
+    longest_boat = Boat.longest
+    longest_boat.first.classifications
+  end
 end
