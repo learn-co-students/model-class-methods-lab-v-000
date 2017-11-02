0332f45e9abe8a388fea6d41c03a2289d8766870
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-02 11:36:51 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,30 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats
+
+  def self.catamaran_operators
+    includes(:classifications).where(classifications: { name: 'Catamaran' })
+    .references(:classifications)
+  end
+
+  def self.sailors
+    includes(:classifications).where(classifications: { name: 'Sailboat' })
+    .references(:classifications).distinct
+  end
+
+  def self.motorboaters
+    includes(:classifications)
+    .where(classifications: { name: 'Motorboat' }).distinct
+  end
+
+  def self.talented_seamen
+    sailors = self.sailors.pluck(:id)
+    motorboaters = self.motorboaters.pluck(:id)
+    where(id: sailors & motorboaters)
+  end
+
+  def self.non_sailors
+    sailors = self.sailors.pluck(:id)
+    where.not(id: sailors)
+  end
 end
