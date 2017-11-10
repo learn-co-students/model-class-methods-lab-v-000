4e0f1e2417e318c09b35fa6fe6724311258b75a3
lpassamano <leighayna.josele@gmail.com>
2017-11-10 11:45:17 -0500

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,39 @@
+require 'pry'
+
 class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.limit(5)
+  end
+
+  def self.dinghy
+    self.where("length < ?", 20)
+  end
+
+  def self.ship
+    self.where("length >= ?", 20)
+  end
+
+  def self.last_three_alphabetically
+    self.order('name DESC').limit(3)
+  end
+
+  def self.without_a_captain
+    self.where("captain_id IS ?", nil)
+  end
+
+  def self.sailboats
+    self.joins(:classifications).where(classifications: {name: "Sailboat"})
+  end
+
+  def self.with_three_classifications
+    self.joins(:boat_classifications).select(:boat_id).group(:boat_id).having("count(*) IS 3")
+  end
+
+  def self.max_length
+    self.maximum(:length)
+  end
 end
