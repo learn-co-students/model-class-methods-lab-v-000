ed914b5637e2aa81c04543fa7981b25bb71561c6
Mahdi Shadkamfarrokhi <mahdi@allstarcode.org>
2017-11-27 04:55:18 +0000

    night

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,19 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.where(id: Boat.joins(:classifications).select(:captain_id).where("classifications.name = 'Catamaran'"))
+  end
+
+  def self.sailors
+    self.where(id: Boat.joins(:classifications).select(:captain_id).where("classifications.name = 'Sailboat'"))
+  end
+
+  def self.talented_seamen
+    # self.where(id: Boat.joins(:classifications).select(:captain_id).where("classifications.name = 'Sailboat'"))
+  end
+
+  def self.non_sailors
+    self.where(id: Boat.joins(:classifications).select(:captain_id).where("classifications.name != 'Sailboat'"))
+  end
 end
