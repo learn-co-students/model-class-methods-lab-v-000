ba486f1c9ebe9a394289e1bd8fe0ee21454783b3
kyleblee <hello.kylelee@gmail.com>
2017-12-04 21:33:10 -0500

    lab complete

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,23 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"}).distinct
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+  end
+
+  def self.captain_type(type_of_ship)
+    includes(boats: :classifications).where(classifications: {name: type_of_ship}).distinct
+  end
+
+  def self.talented_seamen
+    where("id IN (?)", self.captain_type("Sailboat").pluck(:id) & self.captain_type("Motorboat").pluck(:id))
+  end
+
+  def self.non_sailors
+    where.not("id IN (?)", self.sailors.pluck(:id))
+  end
 end
