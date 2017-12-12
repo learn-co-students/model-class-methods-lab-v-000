d59aa9b91c5a6f1b702c384b0354801dd8cd3bae
Pat  Pignataro <pat6284@msn.com>
2017-12-12 19:13:13 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,34 @@
 class Boat < ActiveRecord::Base
+
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    first_five_boats = self.limit(5).order('id asc')
+  end
+
+  def self.dinghy
+    self.where("length < 20")
+  end
+
+  def self.ship
+    self.where("length > 20")
+  end
+
+  def self.last_three_alphabetically
+    last_3 = self.order("name desc").limit(3)
+  end
+
+  def self.without_a_captain
+    self.where(captain_id: nil)
+  end
+
+  def self.sailboats
+    self.joins(:classifications).where(:classifications => {:name => "Sailboat"})
+  end
+
+  def self.with_three_classifications
+    self.joins(:classifications).group("boat_classifications.boat_id").having("COUNT(boat_classifications.classification_id) == 3")
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
+
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    longest_length = Boat.maximum("length")
+    self.includes(:boats).where(:boats => {:length => longest_length})
+  end
 end
