75ef3385f07856b61576b522e92125d26337f2a3
Adam Tropp <adt6261@gmail.com>
2017-12-20 18:46:05 +0000

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,12 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    joins(:boats).order('boats.length').reverse_order.limit(2)
+  end
 end
