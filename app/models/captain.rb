0f692b9f2269252d53847da10da61f3eee16dca9
github username <github email address>
2018-01-08 11:38:38 -0500

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,28 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+    #all captains whose boat classification include catamaran
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+    #captains whose boat classifications include sailboat
+  end
+
+  def self.motorboaters
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+    #captains whose boat classifications include motorboat
+  end
+
+  def self.talented_seamen
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboaters.pluck(:id))
+    #all where you find the same id returned for both methods??
+  end
+
+  def self.non_sailors
+    where.not("id IN (?)", self.sailors.pluck(:id))
+    #where an id does not exist in sailors
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    all
+    ##all the classification!
+  end
+
+  def self.longest
+    Boat.longest.classifications
+    #return boat with longest classification
+  end
 end
