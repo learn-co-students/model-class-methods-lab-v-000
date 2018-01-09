842baa4c3963342ffdd302cf6e102ee482d2fc05
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-09 18:29:13 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,17 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+  
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+  end
+  
+  def self.talented_seamen
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+  end
+  
+  
 end
