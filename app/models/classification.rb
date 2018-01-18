32da57fcf8980e61395de42a355ab364c857c089
Christina <github email address>
2018-01-17 20:02:51 -0700

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    all
+  end
+
+  def self.longest
+    longest_boat = Boat.order(length: :desc).first
+    longest_boat.classifications
+  end
+
 end
