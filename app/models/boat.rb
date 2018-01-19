03fe1808d103099244e83531e02b8c9531ca7f2a
dwyn <dwayne.harmon@gmail.com>
2018-01-19 14:41:40 -0600

    And we're off!

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,12 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    limit(5)
+  end
+
+  def dinghy
+    where("length <?", 20)
+  end
 end
