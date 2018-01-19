7b8cb83485b81c0c3f3feea0ac71c9ddf351a9bb
dwyn <dwayne.harmon@gmail.com>
2018-01-19 15:46:42 -0600

    Progress

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -7,7 +7,28 @@ class Boat < ActiveRecord::Base
     limit(5)
   end
 
-  def dinghy
+  def self.dinghy
     where("length <?", 20)
   end
+
+  def self.ship
+    where("length >?", 20)
+  end
+
+  def self.last_three_alphabetically
+    order('name DESC').limit(3)
+  end
+
+  def self.without_a_captain
+    where(captain_id: nil)
+  end
+
+  def self.sailboats
+    joins(:classifications).where(:classifications => {:name => "Sailboat"})
+  end
+
+  def self.with_three_classifications
+    joins(:boat_classifications).group(:boat_id).having('count(classification_id) =?', 3)
+  end
+
 end
