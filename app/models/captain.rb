500ce82dfe080afacd24bc01db764871c034e3c2
Phoebe Lau <phoebelau7@gmail.com>
2018-02-02 18:41:38 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,34 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins(boats: :classifications).where('classifications.name = ?', 'Catamaran')
+    #SELECT "captains".* FROM "captains"
+    #INNER JOIN "boats" ON "boats"."captain_id" = "captains"."id"
+    #INNER JOIN "boat_classifications" ON "boat_classifications"."boat_id" = "boats"."id"
+    #INNER JOIN "classifications" ON "classifications"."id" = "boat_classifications"."classification_id"
+    #WHERE (classifications.name = 'Catamaran')
+  end
+
+  def self.sailors
+    joins(boats: :classifications).where('classifications.name = ?', 'Sailboat').uniq
+  end
+
+  def self.motorboats
+    joins(boats: :classifications).where('classifications.name = ?', 'Motorboat').uniq
+  end
+
+  def self.talented_seamen
+
+  end
+
+  def self.non_sailors
+    #self.
+  #  self.joins(boats: :classifications).where('classifications.name != ?', 'Sailboat').uniq
+  #  SELECT DISTINCT "captains".* FROM "captains"
+  #  INNER JOIN "boats" ON "boats"."captain_id" = "captains"."id"
+  #  INNER JOIN "boat_classifications" ON "boat_classifications"."boat_id" = "boats"."id"
+  #  INNER JOIN "classifications" ON "classifications"."id" = "boat_classifications"."classification_id"
+  #  WHERE (NOT (classifications.name = 'Sailboat'))
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    boat = Boat.max_length
+    self.joins(:boats).where('boats.length = ?', boat)
+  end
 end
