d2276f139d3ca8653f57ffa3cb8d723b5453642e
Olena Ageyeva <eageeva@hotmail.com>
2018-02-02 01:19:35 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -8,6 +8,35 @@ class Boat < ActiveRecord::Base
   end
 
   def self.dinghy
-    Boat.where(:length<20)
+    Boat.where("length<20")
   end
+
+  def self.ship
+    Boat.where("length>20")
+  end
+
+  def self.last_three_alphabetically
+    Boat.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    Boat.where(captain: nil)
+  end
+
+  def self.sailboats
+  includes(:classifications).where(classifications: { name: 'Sailboat' })
+  end
+
+  def self.with_three_classifications
+   joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+ end
+
+ def self.non_sailboats
+   where("id NOT IN (?)", self.sailboats.pluck(:id))
+ end
+
+ def self.longest
+   order('length DESC').first
+ end
+
 end
