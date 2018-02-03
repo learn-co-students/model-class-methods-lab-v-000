61a09bfd8710521694a6395e976b4f5f33b26944
Yvonne Dreptate <YvonneDreptate@gmail.com>
2018-02-03 16:16:04 -0500

    all tests passing

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,26 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    #select captian-then boat.id- then boat classification where classification id is catamaran?
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+      includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+  end
+
+  def self.motorboaters
+        includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+  end
+
+  def self.talented_seamen
+    where(id: self.sailors & self.motorboaters)
+  end
+
+  def self.non_sailors
+    where.not("id IN (?)", self.sailors.pluck(:id))
+  end
+
+
 end
