b1ed5178f6745613d56d23edba2d46f93734840a
Hannah Carnes <hecarnes@gmail.com>
2018-02-02 22:45:25 -0500

    7 left

---
+++ app/controllers/boats_controller.rb
@@ -0,0 +1,13 @@
+class BoatsController < ApplicationController
+
+  def index
+    @boats = Boat.all
+  end
+
+  def show
+    @boat = Boat.find(params[:id])
+  end
+
+  def new
+  end
+end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,9 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  class << self
+
+    def catamaran_operators
+    end
+  end
 end
