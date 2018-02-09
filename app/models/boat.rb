faebd241049cbf6ae6582b895e53740b8f5ff2fb
TravisA <github email address>
2018-02-08 19:18:35 -0700

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,51 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+
+  def self.first_five
+  	self.limit(5)
+  	#binding.pry
+  end
+
+  def self.dinghy
+  	self.where("length <?", 20)
+  end
+
+  def self.ship
+  	self.where("length >=?", 20)
+  end
+
+  def self.last_three_alphabetically
+  	self.order("name desc").limit(3)
+  end
+
+  def self.without_a_captain
+  	self.where(captain: nil)
+  end
+
+  def self.sailboats
+  	self.joins(:classifications).where(:classifications => {name: "Sailboat"})
+  end
+
+  def self.with_three_classifications
+  	self.joins(:classifications).group(:boat_id).having("count(*) = 3")
+  end
+
+  def self.two_boats
+  	self.joins(:classifications).group(:boat_id).having("id in (?)")
+  end
+
+  def self.catamarans
+  	self.joins(:classifications).where(:classifications => {name: "Catamaran"})
+  end
+
+  def self.motorboats
+  	self.joins(:classifications).where(:classifications => {name: "Motorboat"})
+  end
+
+  def self.otherboats
+  	self.joins(:classifications).where(:classifications => {name: "Motorboat", name: "Sailboat"})
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+  	self.where(:id => [Boat.catamarans.pluck(:captain_id)])
+  	
+  end
+
+  def self.motorboaters
+  	self.where(:id => [Boat.motorboats.pluck(:captain_id)])
+  end
+
+  def self.sailors
+  	self.where(:id => [Boat.sailboats.pluck(:captain_id)])
+  end
+
+  def self.talented_seamen
+  	self.where("id in (?) and id in (?)", Captain.sailors.pluck(:id), Captain.motorboaters.pluck(:id))
+  end
+
+  def self.non_sailors
+  	self.where.not("id in (?)", Captain.sailors.pluck(:id))
+  end
 end
