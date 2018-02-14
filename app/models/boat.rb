84f29b8b1046405ef397e0afdac5b6c6dd309c8e
AnastasiaBarkett <anastasia.barkett@gmail.com>
2018-02-14 18:43:18 -0500

    Create Boat class method #with_three_classifications

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -26,4 +26,8 @@ class Boat < ActiveRecord::Base
   def self.sailboats
     includes(:classifications).where(classifications: {name: "Sailboat"})
   end
+
+  def self.with_three_classifications
+    joins(:classifications).group("boats.id").having("COUNT(*)=3").select("boats.*")
+  end
 end
