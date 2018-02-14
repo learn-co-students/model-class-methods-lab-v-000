d6cfe526aa56cc6703cd8ed86b4e1e0bb073086d
AnastasiaBarkett <anastasia.barkett@gmail.com>
2018-02-14 18:28:21 -0500

    Create first_five class method for Boat

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,8 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    all.limit(5)
+  end
 end
