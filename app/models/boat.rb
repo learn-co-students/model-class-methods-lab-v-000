dba9de75bfc635b8fbb0e360b1ee0eff3fac5120
AnastasiaBarkett <anastasia.barkett@gmail.com>
2018-02-14 18:35:46 -0500

    Create Boat class method #without_a_captain

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -18,4 +18,8 @@ class Boat < ActiveRecord::Base
   def self.last_three_alphabetically
     all.order(name: :desc).limit(3)
   end
+
+  def self.without_a_captain
+    where(captain_id: nil)
+  end
 end
