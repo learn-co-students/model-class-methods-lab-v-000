38b8b34f68f69cab74778dd572619b84019cc4d5
Brad Carlson <b2carlso@yahoo.com>
2018-02-20 17:23:12 +0000

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,12 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    Classification.all
+  end
+
+  def self.longest
+    Classification.where('id IN (?)',Boat.longest.classifications.pluck(:id))
+  end
 end
