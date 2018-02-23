33ff3f43af3688f944a544a64dcbb35a5c589b0b
Brittany Morris <brittmmendez@gmail.com>
2018-02-23 13:52:28 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,41 +3,50 @@ class Boat < ActiveRecord::Base
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
 
+# returns the first five Boats
   def self.first_five
     #Boat.all[0..4]
     #where("id < 6")
     Boat.all.limit(5)
   end
 
+#returns boats shorter than 20 feet
   def self.dinghy
     where("length < ?", 20)
   end
 
+#returns boats 20 feet or longer
   def self.ship
     where("length >= ?", 20)
   end
 
+# returns last three boats in alphabetical order
   def self.last_three_alphabetically
     Boat.order(name: :desc).limit(3)
   end
 
+#returns boats without a captain
   def self.without_a_captain
     where(captain_id: [nil, ""])
     #where(captain_id: nil)
   end
 
+#returns all boats that are sailboats
   def self.sailboats
     includes(:classifications).where('classifications.name = ?', 'Sailboat')
   end
 
+#returns boats with three classifications
   def self.with_three_classifications
     joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
   end
 
+# returns people who are not captains of sailboats
   def self.non_sailboats
     where("id NOT IN (?)", self.sailboats.pluck(:id))
   end
 
+#goes with longest query in classifications LOC12
   def self.longest
     order('length DESC').first
   end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -2,11 +2,13 @@ class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
 
+# returns all classifications
   def self.my_all
     Classification.all
     #all
   end
 
+#returns the classifications for the longest boat
   def self.longest
     Boat.longest.classifications
   end
