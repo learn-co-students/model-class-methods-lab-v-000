cab53ee12a0bf035813c47a797065c8763b0eab7
Seb Gertz <sebgertz@gmail.com>
2018-02-23 14:06:36 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,36 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    all.limit(5)
+  end
+
+  def self.dinghy
+    where("length < 20")
+  end
+
+  def self.ship
+    where("length > 20")
+  end
+
+  def self.last_three_alphabetically
+    all.order(name: :DESC).limit(3)
+  end
+
+  def self.without_a_captain
+    where(captain_id: nil)
+  end
+
+  def self.sailboats
+    includes(:classifications).where("classifications.name = ?", "Sailboat")
+  end
+
+  def self.with_three_classifications
+    includes(:classifications).having('count("classifications.name") = 3').group("boats.id")
+  end
+
+  def self.longest
+    order('length DESC').first
+  end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,26 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+  end
+
+  def self.motorboaters
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"}).distinct
+  end
+
+  def self.talented_seafarers
+    # & returns the intersection of the two groups
+    where(id: self.sailors & self.motorboaters)
+    # OR: where("id IN (?)", self.sailors.pluck(:id) & self.motorboats.pluck(:id))
+  end
+
+  def self.non_sailors
+    where(id: self.all - self.sailors)
+    # OR: where.not("id IN (?)", self.sailors.pluck(:id))
+  end
 end
