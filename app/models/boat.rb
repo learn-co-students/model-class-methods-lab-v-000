ecf9cd5acbb6e854ae6a25a11738815d529682cf
tpetersen0308 <travis.e.petersen@gmail.com>
2018-03-06 17:20:59 -0800

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,33 @@
+require "pry"
+
 class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    order("id LIMIT 5")
+  end
+
+  def self.dinghy
+    where("length < 20")
+  end
+
+  def self.ship
+    where("length >= 20")
+  end
+
+  def self.last_three_alphabetically
+    order("name DESC LIMIT 3")
+  end
+
+  def self.without_a_captain
+    where("captain_id IS NULL")
+  end
+
+  def self.sailboats
+    sailboat_id = Classification.find_by(:name => "Sailboat").id
+    ids = BoatClassification.where("classification_id = #{sailboat_id}").map(&:boat_id)
+    where(:id => ids)
+  end
 end
