6d73ca577bafa9b272c4935d50f3b54f452b4cbc
Jordan Kline <jordan.kline@gmail.com>
2018-03-14 10:52:13 -0700

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,46 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+
+  	def self.first_five
+  	#   returns the first five Boats (FAILED - 1)
+  		all.limit(5)
+      # take(5)
+  	end
+    
+  	def self.dinghy
+  #   returns boats shorter than 20 feet (FAILED - 2)
+  		where('length < ?', 20)
+    end
+
+  	def self.ship
+  #   returns boats 20 feet or longer (FAILED - 3)
+  		where('length >= ?', 20)
+    end
+
+  	def self.last_three_alphabetically
+  #   returns last three boats in alphabetical order (FAILED - 4)
+  		all.order(name: :desc).limit(3)
+  	end
+  	def self.without_a_captain
+  #   returns boats without a captain (FAILED - 5)
+  		# binding.pry
+  		where(captain_id: nil)
+  	end
+  	def self.sailboats
+  #   returns all boats that are sailboats (FAILED - 6)
+  		# binding.pry
+  		includes(:classifications).where(classifications: {:name => 'Sailboat'})
+  	end
+
+  	def self.with_three_classifications
+      # binding.pry
+  #   returns boats with three classifications (FAILED - 7)
+  		joins(:classifications).group("boats.id").having("COUNT(*) = 3")
+  	end
+
+    def self.longest
+      order('length DESC').first
+    end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,17 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+def self.my_all
+	# Classification::my_all returns all classifications
+	all
+end
+	
+def self.longest
+	# Classification#longest returns the classifications for the longest boat
+	Boat.longest.classifications
+	# join(:boats).where()
+end
+	
+
 end
