60e96344d9409c91eefaf9b1abde120d5738c390
David Towster <djt4xe@virginia.edu>
2018-03-15 05:04:33 -0700

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,16 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    Captain.includes(boats: [:classifications]).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    Captain.includes(boats: [:classifications]).where(classifications: {name: "Sailboat"}).group("captains.id")
+  end
+
+  def self.talented_seafarers
+    Captain.includes(boats: [:classifications]).where(classifications: {name: ["Sailboat", "Catamaran"]}).group("captains.id")
+  end
+
 end
