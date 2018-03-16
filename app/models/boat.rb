a80242043140239ad7ccacf9cfe5e681298ec594
Alexander Urizar <urizaraj@gmail.com>
2018-03-16 11:13:49 -0400

    Add class methods

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,42 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def has_c(name)
+    classification = Classification.find_by(name: name)
+    boat.classifications.include?(classification)
+  end
+
+  def self.first_five
+    all.limit(5)
+  end
+
+  def self.dinghy
+    all.find_all { |boat| boat.length < 20 }
+  end
+
+  def self.ship
+    all.find_all { |boat| boat.length > 20 }
+  end
+
+  def self.last_three_alphabetically
+    all.order_by(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    all.reject(&:captain)
+  end
+
+  def self.sailboats
+    sailboat = Classification.find_by(name: 'Sailboat')
+    all.find_all { |boat| boat.classifications.include?(sailboat) }
+  end
+
+  def self.with_three_classifications
+    all.find_all { |boat| boat.classifications.size == 3 }
+  end
+
+  def self.longest
+    all.order_by(length: :desc).first
+  end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,34 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    catamaran = Classification.find_by(name: 'Catamaran')
+    all.find_all do |captain|
+      captain.boats.any? do |boat|
+        boat.classifications.include?(catamaran)
+      end
+    end
+  end
+
+  def self.sailors
+    all.find_all do |captain|
+      captain.boats.any? { |boat| boat.has_c('Sailboat') }
+    end
+  end
+
+  def self.talented_seafarers
+    motorboat = Classification.find_by(name: 'Motorboat')
+    sailboat = Classification.find_by(name: 'Sailboat')
+    all.find_all do |captain|
+      has_motorboat = captain.boats.any? { |boat| boat.classifications.include?(motorboat) }
+      has_sailboat = captain.boats.any? { |boat| boat.classifications.include?(sailboat) }
+      has_motorboat && has_sailboat
+    end
+  end
+
+  def self.non_sailors
+    all.reject do |captain|
+      captain.boats.any? { |boat| boat.has_c('Sailboat') }
+    end
+  end
 end
