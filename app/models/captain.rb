3f407171b040b80a085363d2b1631450564e1b6c
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-03-19 00:43:42 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,8 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    binding.pry
+    self.joins(:classifications).group('captain.id').where("classifications.name = Catamaran")
+  end
 end
