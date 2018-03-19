318b175d0821c61e0ee31ed999304c683792b794
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-03-19 22:04:25 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,7 +2,19 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
+    self.joins(boats: [:classifications]).where("classifications.name = 'Catamaran'")
+  end
+
+  def self.sailors
+    self.joins(boats: [:classifications]).where("classifications.name = 'Sailboat'").uniq
+  end
+
+  def self.talented_seafarers
+    self.joins(boats: [:classifications]).group("classifications.id").having("classifications.name = 'Sailboat' OR classifications.name = 'Motorboat'")
+  end
+
+  def self.non_sailors
     binding.pry
-    self.joins(:classifications).group('captain.id').where("classifications.name = Catamaran")
+    self.joins(boats: [:classifications]).group("captains.name").having("classifications.name != 'Sailboat'")
   end
 end
