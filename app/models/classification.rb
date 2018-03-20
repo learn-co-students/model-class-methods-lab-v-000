8476fd5a457fd9aafc33833783d069e79d13f609
independnt <gonzalezdennis6@gmail.com>
2018-03-20 18:45:31 -0400

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+
+  def self.my_all
+    self.all.distinct
+  end
+
+  def self.longest
+    longest = Boat.longest.pluck(:name)
+    self.joins(:boats).where("boats.name = ?", longest)
+  end
 end
