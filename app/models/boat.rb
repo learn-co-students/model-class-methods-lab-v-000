3c23461c93aa0aa41bebd08158be8f2ffc5ca4a7
funemployeddad <funemployeddad@gmail.com>
2018-03-21 12:23:45 -0700

    returns right in console, but issue is with pluck?

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,20 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    @boats = Boat.all
+    @boats[0..4]
+  end
+
+  def self.dinghy
+    @boats = Boat.all
+    dinghy = []
+    @boats.collect do |boat|
+      if boat.length <= 20
+        dinghy << boat
+      end
+    end
+  end
+
 end
