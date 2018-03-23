f3ef73baa255b21ffb22785db66b9cd64d49f9bc
Adam <github email address>
2018-03-23 11:37:08 -0600

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,47 @@
 class Boat < ActiveRecord::Base
+# t.string  :name
+# t.integer :length
+# t.integer :captain_id
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+# returns the first five Boats
+  def self.first_five
+    Boat.all.limit(5)
+  end
+
+# returns boats shorter than 20 feet
+  def self.dinghy
+    Boat.where("length < ?", 20)
+  end
+
+# returns boats 20 feet or longer
+  def self.ship
+    Boat.where("length >= ?", 20)
+  end
+
+# returns last three boats in alphabetical order
+  def self.last_three_alphabetically
+    Boat.order('name DESC').limit(3)
+  end
+
+# returns boats without a captain
+  def self.without_a_captain
+    Boat.where(:captain => nil)
+  end
+
+# returns all boats that are sailboats
+  def self.sailboats
+    Classification.where(:name => 'Sailboat').first.boats
+  end
+
+# returns boats with three classifications
+  def self.with_three_classifications
+    boats = Boat.all.select do |boat|         #returns array needs to return activerecord relation?
+      boat.classifications.size == 3
+    end
+
+    Boat.where(id: boats.map(&:id))
+  end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,38 @@
 class Captain < ActiveRecord::Base
+# t.string  :name
+# t.boolean :admiral
   has_many :boats
+
+# returns all captains of catamarans
+  def self.catamaran_operators
+    Captain.where(id: Classification.where(:name => "Catamaran").first.boats.pluck(:captain_id))
+  end
+
+# returns captains with sailboats
+  def self.sailors
+    Captain.where(id: Classification.where(:name => "Sailboat").first.boats.pluck(:captain_id))
+  end
+
+# returns captains of motorboats and sailboats
+  def self.talented_seafarers
+    sailboatID = Classification.find_by(:name => 'Sailboat').id
+    motorboatID = Classification.find_by(:name => 'Motorboat').id
+
+    sailboats = Boat.all.select do |boat|
+      boat.classifications.pluck(:id).include?(sailboatID)
+    end
+    motorboats = Boat.all.select do |boat|
+      boat.classifications.pluck(:id).include?(motorboatID)
+    end
+
+    sailboatCaptains = sailboats.collect {|boat| boat.captain}.uniq
+    motorboatCaptains = motorboats.collect {|boat| boat.captain}.uniq
+
+    Captain.where(id: (sailboatCaptains & motorboatCaptains).map(&:id))
+  end
+
+# returns people who are not captains of sailboats
+  def self.non_sailors
+    Captain.where.not(id: Classification.where(:name => "Sailboat").first.boats.pluck(:captain_id))
+  end
 end
