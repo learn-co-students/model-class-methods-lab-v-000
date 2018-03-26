d5ecdb5c1b2a8d2852c93de33e02eb2664673f7d
funemployeddad <funemployeddad@gmail.com>
2018-03-26 15:36:44 -0700

    fixed associations with captains, now should be able to finish. hopefully

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,11 +1,10 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :boat_classifications, through: :boats
+  has_many :classifications, through: :boat_classifications
 
   def self.catamaran_operators
-    # self.select("name").joins("boats").joins("classifications").where("classifications.name == ?", 'Catamaran')
-
-
-    self.joins("boats").joins("boat_classifications").joins("classifications").where("classifications.name == ?", 'Catamaran')
+    self.joins(:classifications).where("classifications.name == ?", "Catamaran")
   end
 
 end
