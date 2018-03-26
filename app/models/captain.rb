9b8eeee97d43717dc263c8e6dc41889049179169
funemployeddad <funemployeddad@gmail.com>
2018-03-25 21:29:59 -0700

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,6 +2,6 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    # where("boats == ?", 'Catamaran')
+    self.joins("boats").joins("boat_classifications").joins("classifications").where("classifications.name == ?", 'Catamaran')
   end
 end
