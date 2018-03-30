ea387f548e4e3c365265a4e3e358cb13113cb668
jacobsilver2 <jacobsilver2@mac.com>
2018-03-30 07:25:04 -0400

    dfdf

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,10 +2,11 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    includes(:boats).where()
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
   end
 
   def self.sailors
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"})
   end
 
   def self.talented_seafarers
