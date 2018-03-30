cb3cfd926c95f08764b2cb957a020eebc6c9e71a
jacobsilver2 <jacobsilver2@mac.com>
2018-03-30 06:14:41 -0400

    second

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,11 +4,29 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    names = Boat.all.map{|b| b.name }[0 .. 4]
+    # names = Boat.all.map{|b| b }[0 .. 4]
+    all.limit(5)
   end
 
   def self.dinghy
-    
+    where("length < 20")
+  end
+
+  def self.ship
+    where("length >= 20")
+  end
+
+  def self.last_three_alphabetically
+    all.sort_by{|b| b.name.downcase}.last(3)
+  end
+
+  def self.without_a_captain
+  end
+
+  def self.sailboats
+  end
+
+  def self.with_three_classifications
   end
 
 
