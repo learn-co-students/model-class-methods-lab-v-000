66ecf122e2d6bda1bd39a4584aae8caf201e9a35
jacobsilver2 <jacobsilver2@mac.com>
2018-03-30 07:51:03 -0400

    ddfgsdf

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,7 +2,11 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"}).uniq
+  end
+
+  def self.motorboat_operators
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"}).uniq
   end
 
   def self.sailors
@@ -10,9 +14,14 @@ class Captain < ActiveRecord::Base
   end
 
   def self.talented_seafarers
-    includes(boats: :classifications).where(classifications: {name: "Sailboat", name:"Motorboat"}).uniq
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
   end
 
-  def self.non_sailors   
+  def self.non_sailors
+    where.not("id IN (?)", self.sailors.pluck(:id)).uniq
   end
+
+  
 end
+
+
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    all
+  end
+
+  def self.longest
+    includes(:boats).where(boats: {"length", "value DESC"}).first
+  end
 end
+
