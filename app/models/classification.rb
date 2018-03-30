efef5398bc6b757559ab3b4e5fbfde28adff8e22
jacobsilver2 <jacobsilver2@mac.com>
2018-03-30 07:52:48 -0400

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -7,7 +7,7 @@ class Classification < ActiveRecord::Base
   end
 
   def self.longest
-    includes(:boats).where(boats: {"length", "value DESC"}).first
+    Boat.longest.classifications
   end
 end
 
