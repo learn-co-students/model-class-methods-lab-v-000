0a274332a0e83faad25ac87f27f347a022dab013
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-04-01 18:16:18 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,12 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+  
+  def self.first_five
+    Boat.all.limit(5)
+  end
+  
+  def self.dinghy
+    Boat.all.where("length < 20")
+  end
 end
