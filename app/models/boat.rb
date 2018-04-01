89ff4ccfd545dd9e29465ceaf9946bdeb24802eb
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-04-01 18:53:41 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -18,4 +18,12 @@ class Boat < ActiveRecord::Base
   def self.last_three_alphabetically
     Boat.all.order("name desc").limit(3)
   end
+  
+  def self.without_a_captain
+    Boat.all.where(captain_id: [nil, ""])
+  end
+  
+  def self.sailboats
+    Boat.all.where("Classification.name = sailboat")
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,10 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+  
+  def self.my_all
+    Classification.all.uniq
+ 
+  end
+
 end
