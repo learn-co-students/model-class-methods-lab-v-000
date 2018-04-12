a76a20bd89d856ab9057550377eedfecc2f5c052
ronald vilorio <soccer23415@gmail.com>
2018-04-12 20:12:16 +0000

    added class method to Captain model

--- app/helpers/captains_helper.rb
+++ app/helpers/captains_helper.rb
@@ -1,2 +1,3 @@
 module CaptainsHelper
+  
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,11 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    binding.pry
+    self.all.each do |captain|
+
+    end
+
+  end
 end
