22168259f022e347a052ba6aa384daa355b6dd3a
Chely Ho <adept_inept@yahoo.com>
2018-04-13 18:40:31 +0000

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    binding.pry
+    boat = Boat.order(length: :desc).limit(1)
+    boat.classifications
+  end
 end
