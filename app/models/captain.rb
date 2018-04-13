329488465e24bfa0e35f417beefb4574cbfafad4
Chely Ho <adept_inept@yahoo.com>
2018-04-13 21:39:55 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -9,13 +9,17 @@ class Captain < ActiveRecord::Base
     self.joins(boats: [:classifications]).where(classifications: {name: 'Sailboat'}).distinct
   end
 
+  def self.motorists
+    self.joins(boats: [:classifications]).where(classifications: {name: 'Motorboat'}).distinct
+  end
+
   def self.talented_seafarers
-    self.sailors.and.self.joins(boats: [:classifications]).where(classifications: {name: 'Motorboat'})
+    # self.sailors & self.motorists union but is an array. the test uses pluck, so have to convert it to activerecord:relation via this ugliness... 
+    self.where(id: (self.sailors & self.motorists).map(&:id))
   end
 
 #breaking bc it's testing for any of the boats??
   def self.non_sailors
-    binding.pry
-    self.joins(boats: [:classifications]).where(classifications: {name='Sailboat'})
+    self.where.not(id: self.sailors.map(&:id))
   end
 end
