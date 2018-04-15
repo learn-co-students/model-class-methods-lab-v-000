2e28f257e8b575f2d79f131968d2886cc8c4dab7
ronald vilorio <soccer23415@gmail.com>
2018-04-15 19:38:00 +0000

    added talented_seafarers method in captain model

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,6 +2,17 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    self.joins(boats: :classifications).where('boat.classifications.name = ?', 'Catamaran')
+    self.joins(boats: :classifications).where(classifications: {name: 'Catamaran'}).uniq
+  end
+  def self.sailors
+
+    self.joins(boats: :classifications).where(classifications: {name: 'Sailboat'}).uniq
+  end
+  def self.talented_seafarers
+    self.joins(boats: :classifications).where(classifications: {name: ['Sailboat', 'Motorboat']}).group('classifications.id').uniq
+  end
+  def self.non_sailors
+    # binding.pry
+    self.joins(boats: :classifications).where('classifications.name != ?', 'Sailboat').order('classifications.id').uniq
   end
 end
