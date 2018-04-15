d8ec8a6cad0abce0682e73f936704aaf4bb3d8c1
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-15 10:58:54 -0700

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,30 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+  def self.first_five
+  Boat.all.limit(5)
+  end
+  def self.dinghy
+    Boat.where("length < 20")
+  end
+  def self.ship
+    Boat.where("length >= 20")
+  end
+  def self.last_three_alphabetically
+    Boat.order(name: :desc).limit(3)
+  end
+  def self.without_a_captain
+    Boat.where(captain_id: nil)
+  end
+  def self.sailboats
+    #Boat.where(classifications.include?(name:"sailboat"))
+
+        includes(:classifications).where(classifications: { name: 'Sailboat' })
+    end
+    def self.longest
+    #  Boat.order(length: :desc).limit(1) #doesnt work because returns a relation not the boat instnce
+      Boat.order(length: :desc).first #works returns boat instance
+
+    end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,8 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  def self.non_sailors #captains who don't have a sailboat (where the classification's name of the boat isnt a sailboat)
+
+  end
+  def self.sailors
+  end
 end
