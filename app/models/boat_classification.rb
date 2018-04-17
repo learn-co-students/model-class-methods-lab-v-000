b1b309829b1cc5fed2a832c6f19d1079e812b13c
John Mooney <jrmint46@gmail.com>
2018-04-17 03:06:05 +0000

    Done.

--- app/models/boat_classification.rb
+++ app/models/boat_classification.rb
@@ -1,4 +1,9 @@
 class BoatClassification < ActiveRecord::Base
   belongs_to :boat
   belongs_to :classification
+
+  def self.find_by_number_of_classifications(number)
+    h = self.group(:boat_id).count(:id).select{ |k, v| v == number }
+    h.keys
+  end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,23 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.where(id: Boat.catamarans.pluck(:captain_id))
+  end
+
+  def self.sailors
+  	where(id: Boat.sailboats.pluck(:captain_id))
+  end
+
+  def self.motorboaters
+  	where(id: Boat.motorboats.pluck(:captain_id))
+  end
+
+  def self.talented_seafarers
+    self.where(id: motorboaters.ids & sailors.ids)
+  end
+
+  def self.non_sailors
+    self.where.not(id: Boat.sailboats.pluck(:captain_id))
+  end
 end
