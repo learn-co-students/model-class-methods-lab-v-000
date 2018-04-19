def0ea677089b0de793f19f2d7d3683111d7dfc2
rockychiang <rocky@chem.ucla.edu>
2018-04-19 06:04:14 -0400

    Finished the lesson

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    self.includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def self.motorboater
+    self.includes(boats: :classifications).where(classifications: {name: "Motorboat"}).uniq
+  end
+
+  def self.talented_seafarers
+    self.where("id IN (?)", self.sailors.pluck(:id) & self.motorboater.pluck(:id))
+  end
+
+  def self.non_sailors
+    self.where.not("id IN (?)", self.sailors.pluck(:id))
+  end
+
 end
