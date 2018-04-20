d9baf4955283f92555863c1b6ee0d6019ee3d4d4
badlychadly <chadmontoya21@yahoo.com>
2018-04-19 21:33:09 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -5,9 +5,29 @@ class Boat < ActiveRecord::Base
 
 
   def self.first_five 
-    binding.pry
-    where("id <= 5")
+    order(created_at: :asc).limit(5)
   end
 
+  def self.dinghy
+    where("length < ?", 20)
+  end
+
+  def self.ship
+    where("length > ?", 20)
+  end
+
+  def self.last_three_alphabetically
+    order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    where(captain_id: nil)
+  end
+
+  def self.sailboats
+    includes(:classifications).where(classifications: {name: "Sailboat"})
+  end
+
+  
 
 end
