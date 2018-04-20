9dfc38e2bb8206224dd9ad41bdba51916bb0deba
github emanao <github emanao@me.com>
2018-04-20 14:32:57 -0400

    passing boat spec tests

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,12 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name:"Catamaran"}).distinct
+  end
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name:"Sailboat"}).distinct
+  end
+  def self.talented_seafarers
+    Captain.includes(boats: :classifications).where("classifications.name" => ["Sailboat","Motorboat"])
+  end
 end
