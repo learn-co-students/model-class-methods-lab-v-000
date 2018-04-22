ee7ff13ec87c70c9bb8a5518da80d3bebcceeade
EC2 Default User <ec2-user@ip-172-31-15-206.us-east-2.compute.internal>
2018-04-22 21:54:56 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,20 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  
+  def self.catamaran_operators
+      joins(:boats).where('boats.id' => Boat.catamarans.ids)
+  end
+  
+  def self.sailors
+      joins(:boats).where('boats.id' => Boat.sailboats.ids).uniq
+  end
+  
+  def self.talented_seafarers
+    where(id: self.joins(:boats).where('boats.id' => Boat.sailboats.ids) & self.joins(:boats).where('boats.id' => Boat.motorboats.ids))
+  end
+  
+  def self.non_sailors
+     where.not(id: self.sailors.ids) 
+  end
+  
 end
