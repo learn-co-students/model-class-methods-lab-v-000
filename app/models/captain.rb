b38764185db130abf3ae58ec8237fc31043e3c82
chaimoosh <chaimoosh@gmail.com>
2018-05-04 13:20:10 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,19 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats
+  def self.catamaran_operators
+    self.joins(:classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    self.joins(:classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def self.talented_seafarers
+    self.joins(:classifications).where(classifications: {name: "Sailboat" AND "Motorboat"}).uniq
+  end
+
+  def self.non_sailors
+    self.joins(:classifications).where.not(classifications: {name: "Sailboat"})
+  end
 end
