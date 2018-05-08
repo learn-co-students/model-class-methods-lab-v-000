d7039b9d6e8500b8393ac8f40c6727f81e722b86
Sean Oughton <seanoughton@gmail.com>
2018-05-08 22:03:47 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,6 +2,18 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    self.includes(:boats).where('boats.classifications.name = ?', 'Catamaran').references(:boats)
+    #byebug
+    #return all captains whose boats are classified as catamarans
+    #you need the classifications for each boat :boat_classifications table
+    #you need the classification name :classifications table
+    #you need to limit the list by classification name : 'Catamaran'
+    #you need to return a list of captains
+
+    #the boat has a list of it's classifications
+    #a captiains boats, each knows about it's classifications
+    #joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+
+    includes(:classifications).where(classifications: { name: 'Catamaran' })
+
   end
 end
