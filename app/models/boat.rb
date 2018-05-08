a05a19b16f53ae4f215019987b0827db47479096
Sean Oughton <seanoughton@gmail.com>
2018-05-08 20:09:45 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -5,6 +5,7 @@ class Boat < ActiveRecord::Base
 
   def self.first_five
     self.limit(5)
+    #all of the boats limited to just the first five
   end
 
   def self.dinghy
@@ -13,13 +14,20 @@ class Boat < ActiveRecord::Base
 
   def self.ship
     self.where("length >= '20'")
+    #all of the boats where the length attribute of the boat is > 20
   end
 
   def self.last_three_alphabetically
     self.order(name: :desc).limit(3)
+    #all of the boats, ordered by name, limited to 3
   end
 
   def self.without_a_captain
     self.where(captain: nil)
+    #all of the boats where the captain attribute is nil
+  end
+
+  def self.sailboats
+    self.includes(:classifications).where('classifications.name = ?', 'Sailboat').references(:classifications)
   end
 end
