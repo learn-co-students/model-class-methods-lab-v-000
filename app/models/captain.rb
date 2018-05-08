c08901a7c1a0a725c49a1aadd01b14d9e6128030
Sean Oughton <seanoughton@gmail.com>
2018-05-08 21:18:01 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,8 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def catamaran_operators
+    byebug
+    self.includes(:boats).where('boats.classifications = ?', 'catamaran').references(:boats)
+  end
 end
