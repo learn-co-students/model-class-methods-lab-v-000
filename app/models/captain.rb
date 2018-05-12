99880f18a8de99381b3ba989630c25b1e123c1ce
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-05-12 14:03:03 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name: 'Catamaran'})
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: 'Sailboat'}).uniq
+  end
+
+  def self.motor_boats
+    includes(boats: :classifications).where(classifications: {name: 'Motorboat'}).uniq
+  end
+
+  def self.talented_seafarers
+    ids = sailors.pluck(:id) & motor_boats.pluck(:id)
+    where(id: ids)
+  end
+
+  def self.non_sailors
+    where.not(id: self.sailors.pluck(:id))
+  end
 end
