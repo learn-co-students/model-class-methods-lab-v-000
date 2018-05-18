b18628792a831b6f06824e5c060fe1f5ad991b8e
johnwojo <jwojo13@gmail.com>
2018-05-18 16:12:29 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -18,17 +18,22 @@ class Boat < ActiveRecord::Base
   end
 
   def self.last_three_alphabetically
+    all.order(name: :desc).limit(3)
   end
 
   def self.without_a_captain
+    self.where(captain_id: nil)
   end
 
   def self.sailboats
+      self.includes(:classifications).where(classifications: {name: 'Sailboat'})
   end
 
   def self.with_three_classifications
+    self.joins(:classifications).group("boats.id").having("COUNT(*)=3").select("boats.*")
   end
 
   def self.longest
+    order(length: :desc).limit(1)
   end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,15 +2,15 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    includes(boats: classifications).where(classifications: {name: "Catamaran"})
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
   end
 
   def self.sailors
-    includes(boats: classifications).where(classifications: {name: "Sailboat"})
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
   end
 
   def self.motorboat_operators
-    includes(boats: classifications).where(classifications: {name: "Motorboat"})
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
   end
 
   def self.talented_seafarers
