7a217a6d5f387d1b2569a0043b15840bc4db4421
Susan Fowler <infopro94@gmail.com>
2018-05-21 10:03:35 -0500

    solved final error in classifications

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -7,8 +7,7 @@ class Classification < ActiveRecord::Base
 	end
 
 	def self.longest
-		Classification.order(length: :desc).limit(1)
-    	# Boat.longest.classifications
-	end
+		Boat.longest.classifications
+    end
 
 end
