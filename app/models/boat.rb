c32e48707f4c719d3d47fbb99cf11bb125058de0
Aubrey Neimeier <neimeier.1@osu.edu>
2018-05-22 23:38:56 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,28 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+  def self.first_five
+    all.limit(5)
+    #.map {|b| b.name}
+  end
+
+  def self.dinghy
+    all.where("length <= ?", 20)
+  end
+
+  def self.ship
+    all.where("length > ?", 20)
+  end
+
+  def self.last_three_alphabetically
+    all.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    all.where("captain_id is NULL")
+  end
+
+  def self.sailboats
+    Boat.joins(:classifications).where(classifications: {name: "Sailboat"})
+  end
 end
