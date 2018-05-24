a2ffdc5a59881458e43b04b697a475af725c875a
Aellon Krider <aellonkrider@gmail.com>
2018-05-24 10:05:34 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,36 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+  	self.all.limit(5)
+  end
+
+  def self.dinghy
+  	self.all.where("length < 20")
+  end
+
+  def self.ship
+  	self.all.where("length >= 20")
+  end
+
+  def self.last_three_alphabetically
+  	self.all.limit(3).order("name DESC")
+  end
+
+  def self.without_a_captain
+  	self.all.where(captain_id: nil)
+  end
+
+  def self.sailboats
+  	self.all.joins(:classifications).where(classifications: { name: 'Sailboat' })
+  end
+
+  def self.with_three_classifications
+  	self.all.joins(:classifications).group("boats.id").having("COUNT(*) = 3")
+  end
+
+  def self.longest
+    self.all.order("length DESC").first
+  end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats
+
+  def self.catamaran_operators
+  	includes(:classifications).where(classifications: { name: 'Catamaran' })
+  end
+
+  def self.sailors
+  	includes(:classifications).where(classifications: { name: 'Sailboat' }).uniq
+  end
+
+  def self.motorboat_captains
+  	includes(:classifications).where(classifications: { name: 'Motorboat' }).uniq
+  end
+
+  def self.talented_seafarers
+  	where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_captains.pluck(:id))
+  end
+
+  def self.non_sailors
+  	where.not("id IN (?)", self.sailors.pluck(:id))
+  end
 end
