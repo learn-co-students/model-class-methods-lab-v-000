39f794118c04da47877080a0f4ed0ee022a075b6
Raiet Intraub <raietsaeed@gmail.com>
2018-06-03 00:07:15 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,6 +2,23 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    Boat.all.where("id <= ?", 5)
+  end
+
+  def self.dinghy
+  end
+
+  def self.ship
+  end
+
+  def self.last_three_alphabetically
+  end
+
+  def self.without_a_captain
+  end
+
 end
 
 # rspec spec/models/boat_spec.rb
