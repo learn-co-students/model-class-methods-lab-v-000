533f74a7d63fd2b6930d212a29db87e8c729cd8d
Raiet Intraub <raietsaeed@gmail.com>
2018-06-03 00:25:37 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,19 +4,28 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    Boat.all.where("id <= ?", 5)
+    puts "get first five boats"
+    Boat.first(5)
   end
 
   def self.dinghy
+    puts "get dinghy?"
+    Boat.where("length < ?", 20)
   end
 
   def self.ship
+    puts "get ship?"
+    Boat.where("length >= ?", 20)
   end
 
   def self.last_three_alphabetically
+    puts "get last three alphabetically"
+    Boat.order(:name).first(3)
   end
 
   def self.without_a_captain
+    puts "get without a captain?"
+    Boat.where("captain_id = ?", nil)
   end
 
 end
