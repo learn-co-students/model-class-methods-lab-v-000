7287d828fd73076c54f09e06a7aba01486a1f5ac
Raiet Intraub <raietsaeed@gmail.com>
2018-06-03 02:53:48 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -21,77 +21,28 @@ class Captain < ActiveRecord::Base
     Captain.joins(boats: :classifications).where(classifications: { name: "Sailboat" }).distinct
   end
 
+  def self.motors
+    # puts "sailors"
+    Captain.joins(boats: :classifications).where(classifications: { name: "Motorboat" }).distinct
+  end
+
   def self.talented_seafarers
     puts "talented_seafarers"
 
-    # NOTE : "Hacked" solution to identify the logic at least
-    seafarers = {}
-    Captain.all.each do |c|
-      c.boats.each do |b|
-        b.classifications.each do |cl|
-          if cl.name == "Sailboat" || cl.name == "Motorboat"
-            # puts "    Classification = #{cl.name}"
-            if !seafarers.has_key?(c.name)
-              # puts "Add seafarer : #{c.name}"
-              seafarers[c.name] = []
-            end
-            if !seafarers[c.name].include?(cl.name)
-              seafarers[c.name] << cl.name
-            end
-          end
-        end
-      end
-    end
-    # puts "Seafarers = #{seafarers}"
-    talented = []
-    seafarers.each do |capt, cl_array|
-      if cl_array.size > 1
-        talented << capt
-      end
-    end
-    # puts "talented = #{talented}"
-    Captain.where(name: talented)
-
-
-
-    # NOTE : Couldn't get this to work... -- ask a question for refactoring/query help
+    sailors = Captain.sailors.pluck(:name)
+    motors = Captain.motors.pluck(:name)
     # expected: ["Captain Cook", "Samuel Axe"]
-    #      got: ["Captain Cook", "Captain Kidd", "William Kyd", "Arel English", "Henry Hudson", "Samuel Axe"]
-    # Captain.joins(boats: :classifications).where(classifications: { name: ["Sailboat", "Motorboat"] }).distinct
 
-    # Captain.joins(boats: :classifications).where('classifications.name = ? AND classifications.name = ?', "Sailboat", "Motorboat")
-    # Captain.joins(boats: :classifications).select("*").group("captains.name, boats.id").having("classifications.name = ? AND classifications.name = ?", "Sailboat", "Motorboat")
+    cross = sailors.include?(motors)
+    puts "cross = #{cross}"
 
   end
 
   def self.non_sailors
-    puts "non_sailors"
-
-    # NOTE : "Hacked" solution to identify the logic at least
+    # puts "non_sailors"
     sailors = Captain.sailors.pluck(:name)
-    puts "Sailors = #{sailors}"
+    # puts "Sailors = #{sailors}"
     Captain.where.not(name: sailors)
-
-    # expected: ["William Kyd", "Arel English", "Henry Hudson"]
-
-    # Captain.all.each do |c|
-    #   puts "Captain = name: #{c.name} || admiral?: #{c.admiral}"
-    #   c.boats.each do |b|
-    #     puts "  Boat = #{b.name}"
-    #     b.classifications.each do |cl|
-    #       if cl.name == "Sailboat" || cl.name == "Motorboat"
-    #         puts "    Classification = #{cl.name}"
-    #       end
-    #     end
-    #   end
-    # end
-
-    # NOTE : Couldn't get this to work... -- ask a question for refactoring/query help
-    # expected: ["William Kyd", "Arel English", "Henry Hudson"]
-    #      got: ["Captain Cook", "Captain Kidd", "William Kyd", "Arel English", "Henry Hudson", "Samuel Axe"]
-    # Captain.joins(boats: :classifications).select("*").group("captains.id").where.not(classifications: { name: "Sailboat" }).distinct
-    # Captain.joins(boats: :classifications).where.not(classifications: { name: "Sailboat" }).distinct
-    # Captain.joins(boats: :classifications).select("captains.name as name, boats.name as boat_name, classifications.name as classification").group("captains.id")
   end
 
 end
