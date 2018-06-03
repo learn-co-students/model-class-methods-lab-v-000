91e79d71e3cb6ab275ae3bbacb72e612fd623ea8
Raiet Intraub <raietsaeed@gmail.com>
2018-06-03 03:15:24 +0000

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -7,11 +7,26 @@ class Classification < ActiveRecord::Base
     Classification.all
   end
 
+  def self.get_boats
+    boats = {}
+    Classification.all.each do |classification|
+      classification.boats.each do |boat|
+        boats[boat.name] = boat.length
+      end
+    end
+    boats = boats.sort_by { |name, length| length }
+    boats
+  end
+
   def self.longest
     puts "get classifications for the longest boat"
-    # Classification.joins(:boats).order(length).limit(1)
+    boats = Classification.get_boats
+    puts "boats = #{boats}"
+    # Classification.joins(:boats).order(boats: length)
   end
 
 end
 
 # rspec spec/models/classification_spec.rb
+
+# https://stackoverflow.com/questions/882070/sorting-an-array-of-objects-in-ruby-by-object-attribute
