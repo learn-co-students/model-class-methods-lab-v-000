dff93f6ac4789e1e5d26e5feed8cd5af04e6c2fd
Raiet Intraub <raietsaeed@gmail.com>
2018-06-03 02:08:10 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -3,6 +3,16 @@ class Captain < ActiveRecord::Base
 
   def self.catamaran_operators
     # puts "catamaran_operators"
+    # Captain.all.each do |c|
+    #   puts "Captain = name: #{c.name} || admiral?: #{c.admiral}"
+    #   c.boats.each do |b|
+    #     puts "Captain Boat = #{b.name}"
+    #     b.classifications.each do |cl|
+    #       puts "Boat Classification = #{cl.name}"
+    #     end
+    #   end
+    # end
+
     Captain.joins(boats: :classifications).where(classifications: { name: "Catamaran" })
   end
 
@@ -13,31 +23,32 @@ class Captain < ActiveRecord::Base
 
   def self.talented_seafarers
     puts "talented_seafarers"
-    # Captain.joins(boats: :classifications).where(classifications: { name: ["Sailboat", "Motorboat"] })
-    # Captain.joins(boats: :classifications).where('classifications.name = ? AND classifications.name = ?', "Sailboat", "Motorboat")
-    Captain.joins(boats: :classifications).select("*").group("captains.name, boats.id").having("classifications.name = ? AND classifications.name = ?", "Sailboat", "Motorboat")
 
-    boats = Captain.joins(boats: :classifications)
-    msboats = boats.select("*").group("captains.name, boats.id").where("classifications.name = ? OR classifications.name = ?", "Sailboat", "Motorboat")
-    mscaptains = msboats.select("*, COUNT(boats.id)").group("captains.name").having("COUNT(boats.id) = 2")
-    puts "mscaptains = #{mscaptains}"
-    mscaptains
+    # expected: ["Captain Cook", "Samuel Axe"]
+    #      got: ["Captain Cook", "Captain Kidd", "William Kyd", "Arel English", "Henry Hudson", "Samuel Axe"]
+    Captain.joins(boats: :classifications).where(classifications: { name: ["Sailboat", "Motorboat"] }).distinct
+    # Captain.joins(boats: :classifications).where('classifications.name = ? AND classifications.name = ?', "Sailboat", "Motorboat")
+    # Captain.joins(boats: :classifications).select("*").group("captains.name, boats.id").having("classifications.name = ? AND classifications.name = ?", "Sailboat", "Motorboat")
   end
 
   def self.non_sailors
     puts "non_sailors"
-    # Captain.all.each do |c|
-    #   puts "Captain = name: #{c.name} || admiral?: #{c.admiral}"
-    #   c.boats.each do |b|
-    #     puts "Captain Boat = #{b.name}"
-    #     b.classifications.each do |cl|
-    #       puts "Boat Classification = #{cl.name}"
-    #     end
-    #   end
-    # end
+    Captain.all.each do |c|
+      puts "Captain = name: #{c.name} || admiral?: #{c.admiral}"
+      c.boats.each do |b|
+        puts "  Boat = #{b.name}"
+        b.classifications.each do |cl|
+          if cl.name == "Sailboat" || cl.name == "Motorboat"
+            puts "    Classification = #{cl.name}"
+          end
+        end
+      end
+    end
+    # expected: ["William Kyd", "Arel English", "Henry Hudson"]
+    #      got: ["Captain Cook", "Captain Kidd", "William Kyd", "Arel English", "Henry Hudson", "Samuel Axe"]
+
     Captain.joins(boats: :classifications).where.not(classifications: { name: "Sailboat" }).distinct
   end
-
 
 end
 
