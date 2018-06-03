2468a0b3bf2a1254e66005ceed6ecaafd91e36e9
Raiet Intraub <raietsaeed@gmail.com>
2018-06-03 01:59:43 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,12 +2,30 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    puts "catamaran_operators"
+    # puts "catamaran_operators"
     Captain.joins(boats: :classifications).where(classifications: { name: "Catamaran" })
   end
 
   def self.sailors
-    puts "sailors"
+    # puts "sailors"
+    Captain.joins(boats: :classifications).where(classifications: { name: "Sailboat" }).distinct
+  end
+
+  def self.talented_seafarers
+    puts "talented_seafarers"
+    # Captain.joins(boats: :classifications).where(classifications: { name: ["Sailboat", "Motorboat"] })
+    # Captain.joins(boats: :classifications).where('classifications.name = ? AND classifications.name = ?', "Sailboat", "Motorboat")
+    Captain.joins(boats: :classifications).select("*").group("captains.name, boats.id").having("classifications.name = ? AND classifications.name = ?", "Sailboat", "Motorboat")
+
+    boats = Captain.joins(boats: :classifications)
+    msboats = boats.select("*").group("captains.name, boats.id").where("classifications.name = ? OR classifications.name = ?", "Sailboat", "Motorboat")
+    mscaptains = msboats.select("*, COUNT(boats.id)").group("captains.name").having("COUNT(boats.id) = 2")
+    puts "mscaptains = #{mscaptains}"
+    mscaptains
+  end
+
+  def self.non_sailors
+    puts "non_sailors"
     # Captain.all.each do |c|
     #   puts "Captain = name: #{c.name} || admiral?: #{c.admiral}"
     #   c.boats.each do |b|
@@ -17,14 +35,7 @@ class Captain < ActiveRecord::Base
     #     end
     #   end
     # end
-  end
-
-  def self.talented_seafarers
-    puts "talented_seafarers"
-  end
-
-  def self.non_sailors
-    puts "non_sailors"
+    Captain.joins(boats: :classifications).where.not(classifications: { name: "Sailboat" }).distinct
   end
 
 
