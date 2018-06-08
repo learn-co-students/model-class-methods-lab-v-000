46423b81441f78eaca0772dd4fc5b9d20967cd6f
AlexFriedland <friedlandaj@gmail.com>
2018-06-08 13:17:20 +0200

    cp

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,34 @@
+require 'pry'
 class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.all.limit(5)
+  end
+
+  def self.dinghy
+    self.all.where("length < ?", 20)
+  end
+
+  def self.ship
+    self.all.where("length >= ?", 20)
+  end
+
+  def self.last_three_alphabetically
+    self.order(name: :desc).limit(3)
+  end
+
+  def without_a_captain
+    #captain_id is null OR blank
+    self.where(captain_id: [nil, ""])
+  end
+
+  def sailboats
+    
+  end
+
+  def with_three_classifications
+  end
 end
