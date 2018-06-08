c7382f1b90c2e08224ecf7412422ab4a4551f853
AlexFriedland <friedlandaj@gmail.com>
2018-06-08 15:55:24 +0200

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -20,15 +20,25 @@ class Boat < ActiveRecord::Base
     self.order(name: :desc).limit(3)
   end
 
-  def without_a_captain
+  def self.without_a_captain
     #captain_id is null OR blank
     self.where(captain_id: [nil, ""])
   end
 
-  def sailboats
-    
+  def self.sailboats
+    self.includes(:classifications).where(classifications: {name: 'Sailboat'})
   end
 
-  def with_three_classifications
+  def self.with_three_classifications
+    #1. joins table on args(:classification)
+    #2. .group => group by attribute(boats.id)
+    #3. .having => (boat.id that has 3 or more classifications)
+    #4. .select("boats.*") => all boats from this group
+    self.joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+  end
+
+  def self.longest
+    #orderby length, descending, take first
+    order('length DESC').first
   end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,27 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    #1. select all captains who have boats /w classifications
+    #2. classifications that have name catamaran
+    self.includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    #same as above, take captain once
+    self.includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def self.motorboat_operators
+    self.includes(boats: :classifications).where(classifications: {name: "Motorboat"}).uniq
+  end
+
+  def self.talented_seafarers
+    #where id INTEGER is all sailors in SAILORS and MOTORBOAT_OPERATORS
+    self.where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
+  end
+
+  def self.non_sailors
+    where.not("id IN (?)", self.sailors.pluck(:id))
+  end
 end
