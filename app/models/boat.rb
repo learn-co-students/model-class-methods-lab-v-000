2cfce8b251567acaf63c07e840c784beaeedbf58
Carol Greene <carolgreene3@gmail.com>
2018-06-14 01:37:43 +0000

    passed first 7 tests

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,28 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    select(:name).limit(5)
+  end
+
+  def self.dinghy
+    select(:name).where("length <=?", 20)
+  end
+
+  def self.ship
+    select(:name).where("length >=?", 20)
+  end
+
+  def self.last_three_alphabetically
+    select(:name).order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    select(:name).where('captain_id is null')
+  end
+
+  def self.sailboats
+    joins(:classifications).where(classifications: { name: 'Sailboat'})
+  end
 end
