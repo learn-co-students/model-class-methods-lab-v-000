2b9be6f39aa625b2f7b4a55204db5622f8b0c866
Deborah Seow <deborahseow9@gmail.com>
2018-06-16 15:49:21 -0500

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,24 +3,52 @@ class Boat < ActiveRecord::Base
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
 
-  # ALSO WORKS
-  #def self.first_five
+  # def self.first_five
   #   all.first(5)
   # end
+  #DOESN'T WORK - not an ActiveRecord method
 
   def self.first_five
     all.limit(5)
   end
 
+
+    # def self.dinghy
+    #   all.select {|boat| boat.length < 20}
+    # end
+    #SELECT doesn't access the DB!
+
   def self.dinghy
-    all.select {|boat| boat.length < 20}
+    where("length < 20")
   end
 
+  # def self.dinghy
+  #   select("length < 20")
+  # end
+
+  #SELECT access DB but doesn't create conditionals in AR, just selects a set already labeled
+
   def self.ship
-    all.select {|boat| boat.length > 20}
+    where("length > 20")
   end
 
   def self.last_three_alphabetically
-    all.last(3).sort
+    all.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    where(captain: nil)
+  end
+
+  def self.sailboats
+    includes(:classifications).where(classifications: {name: "Sailboat"})
+  end
+
+  def self.with_three_classifications
+    joins(:classifications).group('boats.id').having('COUNT(*) = 3')
+  end
+
+  def self.longest
+    order(length: :desc).first
   end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins(boats: :classifications).where(classifications: {name: 'Catamaran'})
+  end
+
+  def self.motorboat_operators
+    joins(boats: :classifications).where(classifications: {name: 'Motorboat'}).distinct
+  end
+
+  def self.sailors
+    joins(boats: :classifications).where(classifications: {name: 'Sailboat'}).distinct
+    #.uniq instead of .distinct also works here!!
+  end
+
+  def self.talented_seafarers
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
+  end
+
+  def self.non_sailors
+    where.not("id IN (?)", self.sailors.pluck(:id))
+  end
 end
