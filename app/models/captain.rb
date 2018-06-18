bcc9cf7776a93b9f277164b17276a147265d514f
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-18 18:26:01 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,21 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins({boats: :classifications}).where(classifications: {name: "Catamaran"})
+  end
+  
+  def self.sailors
+    joins({boats: :classifications}).where(classifications: {name: "Sailboat"}).uniq
+  end
+  
+  def self.talented_seafarers
+
+    where("id IN (?)", self.sailors.pluck(:id))
+  end
+  
+  def self.non_sailors
+    joins({boats: :classifications}).where.not(classifications: {name: "Sailboat"}).uniq
+  end
+    
 end
