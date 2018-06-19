da855bd078bcf88e19580a29a928cf559e165247
Emily Jirles <30752034+Jirles@users.noreply.github.com>
2018-06-19 09:37:24 -0400

    Add search methods
    .join_classifications, .catamaran_operators, .sailors, .talented_seafarers(doesn't work)

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,20 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  
+  def self.join_classifications
+    joins("INNER JOIN boats b ON b.captain_id = captains.id INNER JOIN boat_classifications bc ON bc.boat_id = b.id INNER JOIN classifications c ON c.id = bc.classification_id")
+  end
+
+  def self.catamaran_operators
+    self.join_classifications.where("c.name = 'Catamaran'")
+  end
+
+  def self.sailors
+    self.join_classifications.where("c.name = 'Sailboat'").distinct
+  end
+
+  def self.talented_seafarers
+    self.join_classifications.group("captains.id").where("c.name = 'Motorboat' AND c.name = 'Sailboat'")
+  end
+
 end
