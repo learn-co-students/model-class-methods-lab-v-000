5b20ed31edd8248eafacf9886b6147c787745287
Emily Jirles <30752034+Jirles@users.noreply.github.com>
2018-06-19 12:43:29 -0400

    Add query methods
    .talented_seafarers(works), .non_sailors(does not work)

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -6,15 +6,23 @@ class Captain < ActiveRecord::Base
   end
 
   def self.catamaran_operators
-    self.join_classifications.where("c.name = 'Catamaran'")
+    join_classifications.where("c.name = 'Catamaran'")
   end
 
   def self.sailors
-    self.join_classifications.where("c.name = 'Sailboat'").distinct
+    join_classifications.where("c.name = 'Sailboat'").distinct
   end
 
   def self.talented_seafarers
-    self.join_classifications.group("captains.id").where("c.name = 'Motorboat' AND c.name = 'Sailboat'")
+    join_classifications.group("captains.id").where("c.name = 'Sailboat' OR c.name = 'Motorboat'").having("COUNT(captains.id) > 1 AND SUM(c.name = 'Sailboat') >= 1 AND SUM(c.name = 'Motorboat') >= 1")
+    # find the captains that command both a motorboat and sailboat
+    # group by boats having classifications of either motorboat or sailboat, then select boats that have the same captain
+  end
+  
+  def self.non_sailors
+    join_classifications.group('captains.id').where("c.name != 'Sailboat'")
+    # SELECT * FROM (join the tables) GROUP BY captains.id WHERE c.name != 'Sailboat' => all captains of non-sailboats
+    # find captains whose ships does not include a sailboat => group by captains id, where their boats do not include a sailboat
   end
 
 end
