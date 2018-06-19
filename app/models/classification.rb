80e7d162ef76084aa90b8d2d9a29bf5216e3835e
Emily Jirles <30752034+Jirles@users.noreply.github.com>
2018-06-19 16:33:38 -0400

    Add class methods queries

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+  
+  def self.my_all
+    select(:name).distinct
+  end 
+
+  def self.longest
+    #max_length = with_boats.select('MAX(boats.length)')
+    with_boats.where("boats.length = ?", with_boats.maximum('length'))
+  end 
+  
 end
