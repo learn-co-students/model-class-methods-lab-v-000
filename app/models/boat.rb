1c90df226388fdf3b652b66b99ea791afd230f2b
ElleeB <laura.trager@gmail.com>
2018-06-22 13:47:53 -0400

    Refactor boat#last_three_alpha failing test

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,21 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    limit(5)
+  end
+
+  def self.dinghy
+    where("length <= 20")
+  end
+
+  def self.ship
+    where("length >= 20")
+  end
+
+  # works in console
+  def self.last_three_alphabetically
+    last(3).sort_by { |boat| boat.name }.reverse
+  end
 end
