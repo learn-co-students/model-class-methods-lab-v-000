2f8e3406ea7a300937b7db02d2e5f38d953d8a77
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-06-22 19:42:54 -0400

    Rspeclemented Captain::sailors

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,6 +2,10 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    Captain.joins(boats: [:classifications]).where('classifications.name = ?', "Catamaran")
+    Captain.joins(boats: [:classifications]).where('classifications.name = ?', "Catamaran").distinct
+  end
+
+  def self.sailors
+    Captain.joins(boats: [:classifications]).where('classifications.name = ?', "Sailboat").distinct
   end
 end
