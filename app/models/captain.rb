92d0a914011129ceeff1a745132a011c7b18c030
ElleeB <laura.trager@gmail.com>
2018-06-23 14:30:33 -0400

    Refactor and problem-solve test failure

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,10 +2,7 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    joins(:boats).group(:captain_id).pluck(:captain_id)
-
-    # SELECT captain_id FROM "captains" INNER JOIN "boats" ON "boats"."captain_id" = "captains"."id" GROUP BY captain_id
-    # => [1, 2, 3, 4, 5, 6] 
+    joins(:boats).group(:captain_id).joins(:classifications)
   end
 
 end
@@ -13,3 +10,10 @@ end
 # joins(:boats).pluck(:id)
 # SELECT "captains"."name" FROM "captains" INNER JOIN "boats" ON "boats"."captain_id" = "captains"."id"
 # => [1, 2, 3, 4, 5, 6, 1, 1, 4, 6]
+
+###
+
+# joins(:boats).group(:captain_id).pluck(:captain_id)
+
+# SELECT captain_id FROM "captains" INNER JOIN "boats" ON "boats"."captain_id" = "captains"."id" GROUP BY captain_id
+# => [1, 2, 3, 4, 5, 6]
