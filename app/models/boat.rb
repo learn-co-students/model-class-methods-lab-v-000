2b570aca83c0fe90d74ca0c3c6d8e57f288e8b26
ElleeB <laura.trager@gmail.com>
2018-06-23 12:21:35 -0400

    Refactor with_three_classifications test failure

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -26,4 +26,8 @@ class Boat < ActiveRecord::Base
   def self.sailboats
     joins(:classifications).where({classifications: { name: "Sailboat" }})
   end
+
+  def self.with_three_classifications
+    joins(:boat_classifications).where.not({boat_classifications: { boat_id: nil }}).pluck(:id)
+  end
 end
