4304506c81589046f8734d929561cf98ac84d7a3
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-06-23 22:26:14 -0400

    Created Captain#talented_seafarers method that produces the right result set, but in the wrong format. Spec not passing yet.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -8,4 +8,24 @@ class Captain < ActiveRecord::Base
   def self.sailors
     Captain.joins(boats: [:classifications]).where('classifications.name = ?', "Sailboat").distinct
   end
+
+  def self.motorboaters
+    Captain.joins(boats: [:classifications]).where('classifications.name = ?', "Motorboat").distinct
+  end
+
+  def self.talented_seafarers
+    # get captains who
+    # 1, are in charge of a motorboat, AND
+    # 2, are in charge of a sailboat
+=begin
+  # this produces the correct result set, but as an Array, which does not respond to .pluck? and so fails the spec
+  sailors.merge(motorboaters)
+  all_sailors = self.sailors
+  all_motorboaters = self.motorboaters
+
+  sailors.select do |sailor|
+    motorboaters.include?(sailor)
+  end
+=end
+  end
 end
