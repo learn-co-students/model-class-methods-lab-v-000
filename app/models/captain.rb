a0ee6fb1895de519a53bf1da273b124420d523cd
ElleeB <laura.trager@gmail.com>
2018-06-25 13:03:37 -0400

    Add #longest

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -20,7 +20,12 @@ class Captain < ActiveRecord::Base
     # 1. find all motorboats and sailboats ids
       # class_ids_array = Classification.where(name: ["Sailboat", "Motorboat"]).pluck(:id) #=> [2, 5]
     # 2. find all boats that are either motorboats or sailboats
-      Boat.joins(:classifications).where({classifications: {name: ["Sailboat", "Motorboat"]}})
+    # class_ids = Classification.where(name: ["Sailboat", "Motorboat"]).pluck(:id)
+    # boat_class_combos = BoatClassification.where(classification_id: class_ids).pluck(:boat_id)
+
+
+
+      Boat.joins(:classifications).where({classifications: {name: ["Sailboat", "Motorboat"]}}).pluck(:boat_id)
     # 3. find captains that have more than one boat
     # 4. captains where at least one of their boats is a motorboat and at least one is a sailboat
 
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,15 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+  # returns the classifications for the longest boat
+    class_ids = Boat.order(length: :asc).last.classification_ids
+    where(id: class_ids)
+  end
+
 end
