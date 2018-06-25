974de7d407934c1fbafebbca2bcc5386c2b7ff9d
ElleeB <laura.trager@gmail.com>
2018-06-25 11:10:27 -0400

    Refactor catamaran_operators test success

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,19 +2,11 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    catamarans = Boat.joins(:classifications).where({classifications: { name: "Catamaran" }})
-    self.where(id: catamarans.select(:captain_id))
+    # 1. find boats classified as catamarans
+    # 2. find those boat's captain_ids
+    ids = Boat.joins(:classifications).where({classifications: { name: "Catamaran" }}).pluck(:captain_id)
+    # 3. find captains with ids matching the boats' captain_ids
+    where(id: ids)
   end
 
 end
-
-# joins(:boats).pluck(:id)
-# SELECT "captains"."name" FROM "captains" INNER JOIN "boats" ON "boats"."captain_id" = "captains"."id"
-# => [1, 2, 3, 4, 5, 6, 1, 1, 4, 6]
-
-###
-
-# joins(:boats).group(:captain_id).pluck(:captain_id)
-
-# SELECT captain_id FROM "captains" INNER JOIN "boats" ON "boats"."captain_id" = "captains"."id" GROUP BY captain_id
-# => [1, 2, 3, 4, 5, 6]
