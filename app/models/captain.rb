10c8ac734a179fa507f3e4ada64d5e7096f65b6c
Aly Alidina <aly_alidina@hotmail.com>
2018-07-03 18:29:07 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,15 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications).where("classifications.name" => "Catamaran")
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where("classifications.name" => "Sailboat").uniq
+  end
+
+  def self.talented_seafarers
+    includes(boats: :classifications).where("classifications.name" => "Talented Seafarer").uniq
+  end
 end
