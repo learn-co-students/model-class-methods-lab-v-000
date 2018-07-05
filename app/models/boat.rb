f19669d8abe15de8d9a8535fcaf370b96491d354
github username <github email address>
2018-07-05 12:52:35 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,11 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+    
+    def self.first_five
+        self.first(4).each do |boat|
+            boat.name
+            #binding.pry
+        end 
+    end 
 end
