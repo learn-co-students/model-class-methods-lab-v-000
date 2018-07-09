da191e43248f7536a3f15c968225694d6e315997
Kaitlin Kelly <kk504408@gmail.com>
2018-07-09 11:43:58 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,20 +2,27 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    Captain.joins(boats: :classifications).where(classifications: { name: "Catamaran" })
+    joins(boats: :classifications).where(classifications: { name: "Catamaran" })
   end
 
   def self.sailors
-    Captain.joins(boats: :classifications).where(classifications: { name: "Sailboat" }).uniq
+    joins(boats: :classifications).where(classifications: { name: "Sailboat" }).uniq
+  end
+
+  def self.motorboats
+    joins(boats: :classifications).where(classifications: { name: "Motorboat" }).uniq
   end
 
   def self.talented_seafarers
-    Captain.joins(boats: :classifications).where(classifications: { name: "Motorboat", name: "Sailboat" }).uniq
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboats.pluck(:id))
+  end
+
+  def self.all_boaters
+    select(:id, :name, :admiral, :created_at, :updated_at).uniq
   end
 
   def self.non_sailors
-    Captain.select(:id, :name, :admiral, :created_at, :updated_at) - Captain.joins(boats: :classifications).where(classifications: { name: "Sailboat" }).uniq
-
+    where("id IN (?)", self.all_boaters.pluck(:id) - self.sailors.pluck(:id))
   end
 
 end
