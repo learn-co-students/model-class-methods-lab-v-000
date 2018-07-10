a4c22ef39ee0ef34534ffdc0567e1f150a2d295c
ryanwevans <ryanwevans@icloud.com>
2018-07-10 16:54:38 -0700

    add #talented_seafarers to Captain model to query sailboat and motorboat operators

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -11,4 +11,16 @@ class Captain < ActiveRecord::Base
     Captain.includes(boats: [:classifications]).where('classification_id = ?', sailboat_class.id).distinct
   end
 
+  def self.motorboat_operators
+    motorboat_class = Classification.find_by(:name => "Motorboat")
+    Captain.includes(boats: [:classifications]).where('classification_id = ?', motorboat_class.id).distinct
+  end
+
+  def self.talented_seafarers
+    # sailboat_class = Classification.find_by(name: "Sailboat")
+    # motorboat_class = Classification.find_by(name: "Motorboat")
+
+    where("id IN (?)", self.motorboat_operators.pluck(:id)).where( "id IN (?)", self.sailors.pluck(:id))
+  end
+
 end
