418bd5aabe9af8c5e2b6f58cc962f527f0b751ea
Ruth <github email address>
2018-07-09 22:02:54 -0400

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,12 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.includes(boats: :classifications).uniq
+  end
+
+  def self.longest
+    self.includes(boats: :classifications).order("boats.length DESC").limit(2).uniq
+  end
 end
