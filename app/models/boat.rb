ae4a7ddc3cae56e9714183248254551811fd3e65
Vanessa Toro <vanetoro84@gmail.com>
2018-07-13 16:10:32 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,17 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.first(5)
+  end
+
+  def self.dinghy
+    self.where("length < ?", 20)
+  end
+
+  def ship
+    self.where("length > ?", 20)
+  end
+
 end
