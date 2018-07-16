c2f8c67dd000088f63dc9e6db2cfda2ef8dc1753
Karl Amoguis <ktamoguis@gmail.com>
2018-07-16 15:12:44 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,25 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications)
+    .where(classifications: {name: 'Catamaran'}).uniq
+  end
+
+    def self.sailors
+      #binding.pry
+      includes(boats: :classifications)
+      .where(classifications: {name: 'Sailboat'}).uniq
+    end
+
+  def self.talented_seafarers
+    includes(boats: :classifications)
+    .where(classifications: {name: 'Motorbat'})
+    .where(classifications: {name: 'Sailboat'})
+    .uniq
+  end
+
+  def self.non_sailors
+  end
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,28 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    all
+  end
+
+  def self.longest
+  end
 end
+
+
+#describe Classification do
+#  describe "::my_all" do
+#    it "returns all classifications" do
+#      classifications = ["Ketch", "Sailboat", "Catamaran", "Sloop", "Motorboat", "Center Console", "RIB", "Trawler", "Cat Rig Boat", "Bass Boat", "Pontoon Boat"]
+#      expect(Classification.my_all.pluck(:name).uniq).to eq(classifications)
+#    end
+#  end
+
+#  describe "#longest" do
+#    it "returns the classifications for the longest boat" do
+#      classifications = ["Motorboat", "Center Console"]
+#      expect(Classification.longest.pluck(:name)).to eq(classifications)
+#    end
+#  end
+#end
