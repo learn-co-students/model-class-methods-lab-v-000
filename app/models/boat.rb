cb552207fe2009e00d98e960a8973d278610f11e
lauraBo <BodellLaura@gmail.com>
2018-07-18 16:37:35 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,46 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+
+def self.first_five
+  #{}"returns the first five Boats"
+  all.limit(5)
+end
+
+def self.dinghy
+  #it "returns boats shorter than 20 feet"
+  where("length < 20")
+end
+
+def self.ship
+  #returns boats 20 feet or longer"
+  where("length >= 20")
+end
+
+def self.last_three_alphabetically
+  #{}it "returns last three boats in alphabetical order
+  all.order(name: :desc).limit(3)
+end
+
+def self.without_a_captain
+  #it "returns boats without a captain
+  where(captain_id: nil)
+end
+
+def self.sailboats
+  #returns all boats that are sailboats
+  includes(:classifications).where(classifications: { name: 'Sailboat' })
+end
+
+
+def self.with_three_classifications
+  #returns boats with three classifications
+  joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+end
+
+def self.longest   #returns the longest boat
+  Boat.order(length: :desc).first
+end
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,18 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    #returns all classifications
+    @classifications = Classification.all.uniq
+    @classifications
+  end
+
+
+    #returns the classifications for the longest boat
+
+  def self.longest
+   Boat.longest.classifications
+ end
+
 end
