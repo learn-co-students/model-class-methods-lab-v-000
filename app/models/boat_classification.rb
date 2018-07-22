5af9402b048c236250343da133a033566367eed3
Elle Faraday <faraday.elle@gmail.com>
2018-07-22 19:36:40 +0000

    Done.

--- app/models/boat_classification.rb
+++ app/models/boat_classification.rb
@@ -1,4 +1,8 @@
 class BoatClassification < ActiveRecord::Base
   belongs_to :boat
   belongs_to :classification
+
+  def self.get_classifications_by_boat(boat_id)
+    where(boat_id: boat_id)
+  end
 end
