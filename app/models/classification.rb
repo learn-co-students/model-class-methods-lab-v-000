a1299989793af0f8589dcd8322d07f0e82949298
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-24 20:56:48 +0000

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    self.includes(:boats).limit(2).order('boats.length desc').distinct
+  end
+
 end
