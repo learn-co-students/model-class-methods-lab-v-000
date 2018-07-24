6d9034e0f04f2065847b037c06ee874f6d9a523c
Victoria Meng <nights1ststar@protonmail.com>
2018-07-24 19:44:19 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,34 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    Boat.limit(5)
+  end
+
+  def self.dinghy
+    Boat.where("length < ?", 20)
+  end
+
+  def self.ship
+    Boat.where("length >= ?", 20)
+  end
+
+  def self.last_three_alphabetically
+    Boat.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    Boat.where(captain_id: nil)
+  end
+
+  def self.sailboats
+    Classification.find_by(name: "Sailboat").boats
+  end
+
+  def self.with_three_classifications
+    Boat.joins(:classifications).group(:boat_id).having('count(boat_id) = 3')
+  end
+
+
 end
