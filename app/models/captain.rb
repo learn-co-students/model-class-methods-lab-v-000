c17dc1e332523cbf60f7b7ac5faba8eb0d568196
jebediahelliott <jebediahelliott@gmail.com>
2018-07-26 21:51:28 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,26 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins(boats: :classifications).where("classifications.name = 'Catamaran'")
+  end
+
+  def self.sailors
+    joins(boats: :classifications).where("classifications.name = 'Sailboat'").distinct
+  end
+
+  def self.motorboatists
+    joins(boats: :classifications).where("classifications.name = 'Motorboat'").distinct
+  end
+
+  def self.non_sailors
+    self.where.not(id: self.sailors.pluck(:id))
+  end
+
+  def self.talented_seafarers
+    # select all captains that have a boat with classification "Sailboat" AND classification "Motorboat"
+    # binding.pry
+    self.where(id: self.sailors.pluck(:id) & self.motorboatists.pluck(:id))
+  end
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self
+  end
+
+  def self.longest
+    self.joins(:boats).order("boats.length DESC").limit(2)
+  end
+
 end
