16452a91a714da74ce2bec52e42e22044c2cbf4c
Cristina Lugo <cmlugoce@syr.edu>
2018-07-29 21:56:21 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,18 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+   def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+   def self.sailors
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+   def self.talented_seafarers
+    captains = []
+    sailors.each {|sailor| captains << sailor.id}
+    includes(boats: :classifications).where({classifications: {name: "Motorboat"}, captains: {id: captains}})
+  end
+   def self.non_sailors
+    where.not("id IN (?)", sailors.pluck(:id))
+  end
 end
