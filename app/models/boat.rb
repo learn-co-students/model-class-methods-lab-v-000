361827c7bbcc86adb790099f375ae9106409bcef
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-30 19:51:59 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,16 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    Boat.where("id <= 5")
+  end
+
+  def self.dinghy
+    Boat.where("length < 20")
+  end
+
+  def self.ship
+    Boat.where("length >= 20")
+  end
 end
