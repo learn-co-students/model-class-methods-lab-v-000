783f9b23ae8be052db58d1d0b9b5fafa6ed03c33
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-30 22:13:42 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
@@ -17,5 +18,16 @@ class Boat < ActiveRecord::Base
 
   def self.last_three_alphabetically
     Boat.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    Boat.where(:captain_id=>nil)
+  end
+
+  def self.sailboats
+    sailboat = Classification.find_by(:name => "Sailboat")
+    ids = BoatClassification.find_boats_by_category(sailboat.id)
+    boats = ids.map { |id| Boat.find(id)}
+    Boat.where(id: boats.map(&:id))
   end
 end
--- app/models/boat_classification.rb
+++ app/models/boat_classification.rb
@@ -1,4 +1,9 @@
+require 'pry'
 class BoatClassification < ActiveRecord::Base
   belongs_to :boat
   belongs_to :classification
+
+  def self.find_boats_by_category (category)
+    self.where("classification_id == ?",category).map { | classification | classification.boat_id }
+  end
 end
