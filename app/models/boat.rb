78cfbc697a775466060513de43e3cf5be0290a2f
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-30 23:15:16 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -25,21 +25,17 @@ class Boat < ActiveRecord::Base
   end
 
   def self.sailboats
-    # Convoluted - Ask boat classifications for boat ids with classification sailboat.
-    # then find the corresponding boats (this will be an array)
+    # Convoluted - Ask boat classifications for boats with classification sailboat (this will be an array)
     # then convert to ActiveRecord:Relation so test can use pluck
     sailboat = Classification.find_by(:name => "Sailboat")
-    ids = BoatClassification.find_boats_by_classification(sailboat.id)
-    boats = ids.map { |id| Boat.find(id)}
+    boats = BoatClassification.find_boats_by_classification(sailboat.id)
     Boat.where(id: boats.map(&:id))
   end
 
   def self.with_three_classifications
-    # same convoluted logic - having problem figuring out how to get boats from
-    # classification data and then convert to activerecord:relation
-    list = BoatClassification.find_boats_by_number_classifications (3)
-    ids = list.map { | classification | classification.boat_id }
-    boats = ids.map { |id| Boat.find(id) }
+    # same convoluted logic - having problem figuring out how to get
+    # BoatClassification to return boats as activerecord:relation
+    boats = BoatClassification.find_boats_by_number_classifications (3)
     Boat.where(id: boats.map(&:id))
   end
 end
--- app/models/boat_classification.rb
+++ app/models/boat_classification.rb
@@ -4,10 +4,10 @@ class BoatClassification < ActiveRecord::Base
   belongs_to :classification
 
   def self.find_boats_by_classification (find_classification)
-    self.where("classification_id == ?",find_classification).map { | classification | classification.boat_id }
+    self.where("classification_id == ?",find_classification).map { | classification | classification.boat }
   end
 
   def self.find_boats_by_number_classifications (n)
-    self.group(:boat_id).having('COUNT(*)==?',n)
+    self.group(:boat_id).having('COUNT(*)==?',n).map { | classification | classification.boat }
   end
 end
