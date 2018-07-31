72cc56b5fd00fb8c0f7227c311c975b6d306472c
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-07-31 20:16:42 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,17 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  
+  def self.catamaran_operators
+    Captain.includes(:boats).where("boats.classifications = ?", "Catamaran")
+  end
+  
+  def self.sailors
+  end
+  
+  def self.talented_seafarers
+  end
+  
+  def self.non_sailors
+  end
+  
 end
