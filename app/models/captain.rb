77ccf38fdcf227ad419dfbca7feabd5988b34f7e
taryn yaeger <tsyaeger@gmail.com>
2018-07-31 02:18:21 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -5,38 +5,22 @@ class Captain < ActiveRecord::Base
     @captains = Captain.joins(boats: :classifications).where("classifications.name LIKE ?", "Catamaran")
   end
   
+  def self.sailors
+    @captains = Captain.joins(boats: :classifications).where("classifications.name LIKE ?", "Sailboat").distinct
+  end
+  
+  def self.motorboats
+    @captains = Captain.joins(boats: :classifications).where("classifications.name LIKE ?", "Motorboat").distinct
+  end
+  
+  def self.talented_seafarers
+    @captains = Captain.where(id: self.sailors.pluck(:id) & self.motorboats.pluck(:id))
+  end
+  
+  def self.non_sailors 
+    @captains = Captain.where.not(id: self.sailors.pluck(:id)).distinct
+  end
+  
+  
 end
 
-
-# 
-# Article.joins(comments: :guest)
-
-# describe Captain do
-#   describe "::catamaran_operators" do
-#     it "returns all captains of catamarans" do
-#       captains = ["Captain Kidd", "Samuel Axe"]
-#       expect(Captain.catamaran_operators.pluck(:name)).to eq(captains)
-#     end
-#   end
-
-#   describe "::sailors" do
-#     it "returns captains with sailboats" do
-#       captains = ["Captain Cook", "Captain Kidd", "Samuel Axe"]
-#       expect(Captain.sailors.pluck(:name)).to eq(captains)
-#     end
-#   end
-
-#   describe "::talented_seafarers" do
-#     it "returns captains of motorboats and sailboats" do
-#       captains = ["Captain Cook", "Samuel Axe"]
-#       expect(Captain.talented_seafarers.pluck(:name)).to eq(captains)
-#     end
-#   end
-
-#   describe "::non_sailors" do
-#     it "returns people who are not captains of sailboats" do
-#       captains = ["William Kyd", "Arel English", "Henry Hudson"]
-#       expect(Captain.non_sailors.pluck(:name)).to eq(captains)
-#     end
-#   end
-# end
