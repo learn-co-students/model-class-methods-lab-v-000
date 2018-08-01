169b3e4cf7f769d1c51c8c901a312822e56b0cb9
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-01 17:37:55 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,31 +4,31 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
   
   def self.first_five
-    Boat.limit(5)
+    limit(5)
   end
   
   def self.dinghy
-    Boat.where("length < ?", 20)
+    where("length < ?", 20)
   end
   
   def self.ship
-    Boat.where("length >= ?", 20)
+    where("length >= ?", 20)
   end
   
   def self.last_three_alphabetically
-    Boat.order(:name).reverse_order.limit(3)
+    order(:name).reverse_order.limit(3)
   end
   
   def self.without_a_captain 
-    Boat.where(captain_id: nil)
+    where(captain_id: nil)
   end
   
   def self.sailboats
-    Boat.joins(:classifications).where("classifications.name = ?", "Sailboat")
+    joins(:classifications).where("classifications.name = ?", "Sailboat")
   end
   
   def self.with_three_classifications
-    
+     joins(:classifications).group("boats.id").having("COUNT(*) = 3")
   end
   
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,20 +2,21 @@ class Captain < ActiveRecord::Base
   has_many :boats
   
   def self.catamaran_operators
-    Captain.joins(boats: [:classifications]).where("classifications.name = ?", "Catamaran")
+    joins(boats: [:classifications]).where("classifications.name = ?", "Catamaran")
   end
   
   def self.sailors
-    Captain.joins(boats: [:classifications]).where("classifications.name = ?", "Sailboat").distinct
+    joins(boats: [:classifications]).where("classifications.name = ?", "Sailboat").distinct
   end
   
   def self.talented_seafarers
-   
-    # # Captain.joins(boats: [:classifications]).where("classifications.name = ?", )
+    captains = []
+    sailors.each {|sailor| captains << sailor.id}
+    includes(boats: :classifications).where({classifications: {name: "Motorboat"}, captains: {id: captains}})
   end
   
   def self.non_sailors
-   Captain.joins(boats: [:classifications]).where("classifications.name != ?", "Sailboat").distinct
+    where.not("id IN (?)", sailors.pluck(:id))
     
   end
   
