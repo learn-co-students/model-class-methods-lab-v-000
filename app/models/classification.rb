bcba39419315c005ae399cc223f029ab906a4f82
coryjbergquist <cory.j.bergquist@gmail.com>
2018-08-06 13:36:12 -0700

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    all
+  end
+
+  def self.longest
+    includes(boats: :boat_classifications).order('length DESC').uniq.limit(2)
+  end
+
 end
