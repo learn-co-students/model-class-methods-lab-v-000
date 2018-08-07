d55ca8d3d037868862d4c0269f9cadbb5c0a724d
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-08-07 17:02:41 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,5 +1,6 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
   def self.catamaran_operators
     joins(boats: :classifications).where(classifications: {name: "Catamaran"})
   end
@@ -8,8 +9,16 @@ class Captain < ActiveRecord::Base
     joins(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
   end
 
+  def self.motorboaters
+    joins(boats: :classifications).where(classifications: {name: "Motorboat"}).uniq
+  end
+
   def self.talented_seafarers
-    joins(boats: :classifications).where(classifications: {name: "Sailboat", "Motorboat"}).uniq
+    where(id: sailors & motorboaters).uniq
+  end
+
+  def self.non_sailors
+    where.not(id: sailors)
   end
 
 end
