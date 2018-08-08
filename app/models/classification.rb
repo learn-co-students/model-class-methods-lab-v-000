e5a033d7318b67c273dacf5c6142d26e647a6563
Carrie Savinell <carrielane89@gmail.com>
2018-08-08 19:48:52 +0000

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,15 @@
+require 'pry'
+
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    Classification.all
+  end
+
+  def self.longest
+    binding.pry
+    Boat.order("length DESC").first.classifications
+  end
 end
