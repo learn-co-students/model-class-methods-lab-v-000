f2a0a7b79f6d7bf35f104f73542237f1c6d2fcd7
Elaine Anderson <eparie@bu.edu>
2018-08-09 13:24:14 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,10 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+  Boat.order(:id).limit(5)
+  end
+
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,13 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.all.each do |captain|
+      captain.boats.collect do |boat|
+        if boat.classifications.name == "Catamaran"
+        boat.captain
+      end
+      end
+    end
+  end
 end
