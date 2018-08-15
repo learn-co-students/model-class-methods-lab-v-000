e8ee78be9ba23f513bbc95666ffbb3050685ca7c
Sara Stanton <sara.mpa13@gmail.com>
2018-08-15 01:35:30 -0500

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,23 +4,28 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
+    Boat.all[0..4]
   end
-  
-  def self.dinghy
-  end
-  
+
+ def self.dinghy
+   where("length <= 20" )
+ end
+
   def self.ship
+    where("length > 20" )
   end
 
   def self.last_three_alphabetically
+    Boat.all.sort {|a,b| b.name <=> a.name}.first(3)
   end
-  
+
   def self.without_a_captain
+    where(captain: nil)
   end
-  
+
   def self.sailboats
   end
-  
+
   def self.with_three_classifications
   end
 
--- db/migrate/20140313192355_create_boats.rb
+++ db/migrate/20140313192355_create_boats.rb
@@ -1,4 +1,4 @@
-class CreateBoats < ActiveRecord::Migration
+class CreateBoats < ActiveRecord::Migration[5.2]
   def change
     create_table :boats do |t|
       t.string  :name
