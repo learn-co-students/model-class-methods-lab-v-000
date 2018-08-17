fb7c82cba3ee54b8287dc88d3ce9c3faebbad9f4
Katrina Brinson <kbrinso@gmail.com>
2018-08-17 23:49:34 +0000

    getting started

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,8 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+  def self.first_five
+    binding.pry
+    Boat.first(5)
+  end
 end
