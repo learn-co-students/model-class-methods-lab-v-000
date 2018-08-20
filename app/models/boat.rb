1be0d19fc771b0ccfb6208236ff47a3b1aea4b5a
Veronica Bustabad <vbustabad@gmail.com>
2018-08-20 03:02:36 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,35 @@
+require 'pry'
+
 class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.all.limit(5)
+  end 
+
+  def self.dinghy 
+    where("length < 20")
+  end 
+
+  def self.ship 
+    where("length >= 20")
+  end 
+
+  def self.last_three_alphabetically
+    self.all.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    where(captain_id: nil)
+  end 
+
+  def self.sailboats
+    self.includes(:classifications).where('classifications.name = ?', 'Sailboat').references(:classifications)
+  end 
+
+  def self.with_three_classifications
+    self.joins(:classifications).having('classification_id.count = 3').group('classification_id')
+  end 
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,17 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    # self.includes(boats: :classifications).where('classification.name = ?', Catamaran )
+  end
+
+  def self.sailors
+  end 
+
+  def self.talented_seafarers
+  end 
+
+  def self.non_sailors
+    # self.where(["classification.name = :classification_name and captain_id = :captain_id", { classification_name: "Sailboat", captain_id: false }])
+  end 
 end
