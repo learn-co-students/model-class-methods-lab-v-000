77b35aa641ee7fc56d7d29529fc801474506c541
Katrina Brinson <kbrinso@gmail.com>
2018-08-21 02:30:17 +0000

    Still working on Catamaran

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -5,6 +5,10 @@ class Captain < ActiveRecord::Base
     binding.pry
     #find all captains of boats where classification is catamaran
     self.joins(:boats).joins(:classifications).where(boats: {classifications: {name: "Catamaran"}})
+    Boat.joins(:classifications).where(classifications: {name: "Catamaran"})
+    Boat.joins(:classifications).where(classifications: {name: "Catamaran"}).joins(:captains).where(captains: name)
+
+
     self.joins(:boats).group('captain.id').having('')
     Boat.joins(:classifications).group('boats.id').having('count(boat_id) == 3')
 
