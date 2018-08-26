be8e1bea526cd038c06e9cf092192e234b05f21d
Kayvan Vakili <kvakili813@gmail.com>
2018-08-26 03:20:32 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,32 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+  def self.first_five
+    limit(5)
+  end
+
+  def self.dinghy
+    where('length < 20')
+  end
+
+  def self.ship
+    where('length >= 20')
+  end
+
+  def self.last_three_alphabetically
+    order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    where(captain_id: nil)
+  end
+
+  def self.sailboats
+    self.joins(:classifications).where("classifications.name = ?", "Sailboat")
+  end
+
+  def self.with_three_classifications
+    self.joins(:classifications).group("boats.id").having("count(*)=?", 3)
+  end
+
 end
