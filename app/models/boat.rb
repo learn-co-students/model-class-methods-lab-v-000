d902942937cc021bd58c71db9d6a308d2490ac51
ElvinGarcia <dimix181@gmail.com>
2018-08-27 13:16:48 -0400

    not all test are passing

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,18 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.all.limit(5)
+  end
+
+  def self.dinghy
+     self.having('length < 20').group(:id)
+  end
+
+  def self.ship
+    self.having('length >= 20').group(:id)
+
+  end
+
 end
