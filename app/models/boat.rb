7a5324c872b74b600ca69af9aa84eee26c6a0ff6
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-28 23:21:44 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,14 @@
+
 class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+  
+  def self.first_five
+    
+binding.pry    
+
+    take(5)
+  end 
+  
 end
