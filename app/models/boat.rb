dcfd7e3585407d43780a9b2292e05d78b2407762
Jamie Brown <jmbrowncow@gmail.com>
2018-08-29 21:33:20 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -11,4 +11,37 @@ class Boat < ActiveRecord::Base
     where("length < 20")
   end
 
+  def self.ship
+    where("length >= 20")
+  end
+
+  def self.last_three_alphabetically
+    self.all.order("name DESC").limit(3)
+  end
+
+  def self.without_a_captain
+    where(captain_id: nil)
+  end
+
+  def self.sailboats
+    # Client.all :joins => :orders, :conditions => {:orders => {:created_at => time_range}}
+    sailboatid= Classification.find_by(name: "Sailboat").id
+    self.all.joins(:boat_classifications).where(:boat_classifications=>{:classification_id => sailboatid})
+  end
+
+  def self.with_three_classifications
+    self.all.joins(:classifications).group("boat_id").having("Count(*) = 3")
+  end
+
+  def self.catamarans
+    joins(:classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.motorboats
+    joins(:classifications).where(classifications: {name: "Motorboat"})
+  end
+
+  def self.longest
+    self.maximum("length")
+  end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,28 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins(:boats).merge(Boat.catamarans)
+  end
+
+  def self.sailors
+    joins(:boats).merge(Boat.sailboats).uniq
+  end
+
+  def self.motorboaters
+    joins(:boats).merge(Boat.motorboats).uniq
+  end
+
+  def self.talented_seafarers
+      where("id IN (?)", self.sailors.pluck(:id) & self.motorboaters.pluck(:id))
+      # sail = joins(:boats).merge(Boat.sailboats).uniq
+      # motor =  joins(:boats).merge(Boat.motorboats).uniq
+      # sail & motor
+  end
+
+  def self.non_sailors
+    # self.all - self.sailors
+    where.not("id IN (?)", self.sailors.pluck(:id))
+  end
+
 end
