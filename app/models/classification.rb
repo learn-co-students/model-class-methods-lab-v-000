7e65eccd2a5f99bcfc8be3571195d8ed72980c5e
Cl3arglass <haltaffer@gmail.com>
2018-08-29 16:52:30 -0700

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -7,7 +7,6 @@ class Classification < ActiveRecord::Base
   end
 
   def self.longest
-  	# self.joins(:boats).order("length DESC").limit(2)
-  	self.joins(:boats).longest.my_all
+  	self.joins(:boats).order("length DESC").limit(2)
   end
 end
