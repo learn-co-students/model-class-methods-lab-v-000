4d7f15bb4c9e97370399e836f9b57bd85744fe39
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-30 02:09:40 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -29,10 +29,7 @@ class Boat < ActiveRecord::Base
   end
   
   def self.with_three_classifications
-    # count = 3
-    # t = joins(:classifications).group(:id).having('boats.id = ?', count)
-    # joins(:classifications).group("boats.id")
-binding.pry 
+    joins(:classifications).group("boats.id").having("count(classifications.id) = ?", 3)
   end
   
 end
