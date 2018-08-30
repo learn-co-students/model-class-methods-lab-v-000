930bc43f545cbac9924518aec828f02112c43d03
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-30 04:32:14 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -3,7 +3,7 @@ class Captain < ActiveRecord::Base
   has_many :boats
   
   def self.catamaran_operators
-    joins(boats: :classifications).where(classifications: {name: "catamarans"})
+    joins(boats: :classifications).where(classifications: {name: "Catamarans"})
   end
   
   def self.sailors
