1d0f3a17b797dbd63515c8f4065b30df8eb82032
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-30 03:45:55 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,21 @@
+
 class Captain < ActiveRecord::Base
   has_many :boats
+  
+  def
+    binding.pry
+  end
+  
+  def
+    binding.pry
+  end
+  
+  def
+    binding.pry
+  end
+  
+  def
+    binding.pry
+  end
+  
 end
