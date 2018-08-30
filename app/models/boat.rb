dc79aab9fcb359b3d2cbacced3fb76ba8035cb6e
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-30 04:31:46 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -9,27 +9,27 @@ class Boat < ActiveRecord::Base
   end
   
   def self.dinghy
-    all.where("length < ?", 20)
+    where("length < ?", 20)
   end
   
   def self.ship
-    all.where("length > ?", 20)
+    where("length > ?", 20)
   end
   
   def self.last_three_alphabetically
-    all.order(name: :desc).limit(3)
+    order(name: :desc).limit(3)
   end
   
   def self.without_a_captain
-    all.where(captain: nil)   
+    where(captain: nil)   
   end
   
   def self.sailboats
-    all.includes(:classifications).where(classifications: {name: 'Sailboat'})
+    includes(:classifications).where(classifications: {name: 'Sailboat'})
   end
   
   def self.with_three_classifications
-    all.joins(:classifications).group("boats.id").having("count(classifications.id) = ?", 3)
+    joins(:classifications).group("boats.id").having("count(classifications.id) = ?", 3)
   end
   
 end
