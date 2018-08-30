915f01ff61e6dc7bd94e8b296448160008dd004b
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-30 16:55:09 +0000

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
+
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+  
+  def self.my_all
+binding.pry 
+  end
+  
+  def self.longest
+binding.pry 
+  end
+  
 end
