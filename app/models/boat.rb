14bae240feaf6163297c22589beac16a204f17a9
epic0448 <samuelogundiran@Yahoo.com>
2018-09-05 20:33:35 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,35 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def first_five
+    # returns the first five Boats
+    
+  end
+
+  def dinghy
+    # returns boats shorter than 20 feet
+  end
+
+  def ship
+    # return boats 20 feet or longer
+  end
+
+  def last_three_alphabetically
+    # returns last three boats in alphabetical order
+  end
+
+  def without_a_captain
+    #returns boats wiout a captian
+  end
+
+  def sailboats
+    # returns all boats that are sailboats
+  end
+
+  def with_three_classifications
+    #return boats with three classifications
+  end
+
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,28 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+   def catamaron_operators
+    #  returns all Captains of catamarns
+   end
+
+   def sailors
+     #returns captians with sailboats
+   end
+
+   def talented_seafarers
+    #returns captains of motorboats and sailboats
+   end
+
+   def non_sailors
+    # returns people who are not captains of sailboats
+   end
+
+   def my_all
+    #  returns all classifications
+   end
+
+   def longest returns the classifications for the longest boats
+    # returns the classifications for the longest boat
+   end
+
 end
