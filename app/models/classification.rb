21a3596f4b9ec1236c9d9983182c36bde5d09eb2
rfenix3 <richard.fenix@gmail.com>
2018-09-07 18:09:22 -0700

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    all
+  end
+
+  def self.longest
+    # longest is defined in and called from boat.rb model
+    Boat.longest.classifications
+  end
+
 end
