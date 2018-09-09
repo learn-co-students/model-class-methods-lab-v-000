9e1611f8a4f273662a4406ea72bc2ac42c27c917
enciie <enciie@gmail.com>
2018-09-09 03:44:36 -0700

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,41 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five #returns the first five Boats
+    self.all.limit(5)
+  end
+
+  def self.dinghy #returns boats shorter than 20 feet
+    where("length < 20")
+  end
+
+  def self.ship #returns boats 20 feet or longer
+    where("length >= 20")
+  end
+
+  def self.last_three_alphabetically #returns last three boats in alphabetical order
+    self.all.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain #returns boats without a captain
+    where(captain_id: nil)
+  end
+
+  def self.sailboats #returns all boats that are sailboats
+    includes(:classifications).where(classifications: {name: 'Sailboat'})
+  end
+
+  def self.with_three_classifications
+    joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+  end
+
+  def self.non_sailboats #returns non-sailboats
+    where("id NOT IN (?)", self.sailboats.pluck(:id))
+  end
+
+  def self.longest #returns the classifications for the longest boat
+    order('length DESC').first
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators #returns all captains of catamarans
+    includes(boats: :classifications).where(classifications: {name: 'Catamaran'})
+  end
+
+  def self.sailors #returns captains with sailboats
+    includes(boats: :classifications).where(classifications: {name: 'Sailboat'}).uniq
+  end
+
+  def self.motorboat_operators #returns captains with motorboats
+    includes(boats: :classifications).where(classifications: {name: 'Motorboat'}).uniq
+  end
+
+  def self.talented_seafarers #returns captains of motorboats and sailboats
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
+  end
+
+  def self.non_sailors #returns people who are not captains of sailboats
+    where.not("id IN (?)", self.sailors.pluck(:id))
+  end
+
 end
