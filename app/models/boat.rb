25e240e2dd3d8caf9860a1fa59bdec704c35fb79
Katrina Pritchard <pritckat@gmail.com>
2018-09-27 19:48:38 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,8 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.all[0..4]
+  end
 end
