d0a167ec5441f834cc00065c397adae502d9bf2e
Christine Cua <cloudycua@gmail.com>
2018-10-04 16:26:41 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -7,4 +7,31 @@ class Boat < ActiveRecord::Base
     all.limit(5)
   end
 
+  def self.dinghy
+    all.where("length < '20'")
+  end
+
+  def self.ship
+    all.where("length > '20'")
+  end
+
+  def self.last_three_alphabetically
+    all.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    all.where(captain: nil)
+  end
+
+  def self.sailboats
+    includes(:classifications).where(classifications: { name: 'Sailboat' })
+  end
+
+  def self.with_three_classifications
+    joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+  end
+
+  def self.longest
+    order('length DESC').first
+  end
 end
