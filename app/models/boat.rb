7c3f8835d64455662232a329353c02529fcdc0fe
Christine Cua <cloudycua@gmail.com>
2018-10-04 14:30:26 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,9 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.all.min(5)
+  end
+
 end
