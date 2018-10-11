49e066541297e7e3e3189f5b4a31879d38e69d6d
Everett <everett.bowline@gmail.com>
2018-10-11 21:59:01 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,32 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.limit(5)
+  end
+
+  def self.dinghy
+    where("length < 20")
+  end
+
+  def self.ship
+    where("length > 20")
+  end
+
+  def self.last_three_alphabetically
+    self.limit(3).order('name desc')
+  end
+
+  def self.without_a_captain
+    where(captain_id: nil)
+  end
+
+  def self.sailboats
+    self.includes(:classifications).where('classifications.name = ?', 'Sailboat')
+  end
+
+  def self.with_three_classifications
+    self.joins(:classifications).having('COUNT(classification_id) = 3').group('boat_id')
+  end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,19 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+
+  end
+
+  def self.sailors
+
+  end
+
+  def self.talented_seafarers
+
+  end
+
+  def self.non_sailors
+
+  end
 end
