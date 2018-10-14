4b598224105b0dcc7f57a752eef4f46df5b46dc9
BrettSWeisberg <weisberg.brett@gmail.com>
2018-10-14 15:22:55 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,7 +2,12 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def catamaran_operators
-    
+
   end
+
+  def self.non_sailors
+      Boat.where('captain_id != ?', [nil,""]).each do
+      end
+    end
 
 end
