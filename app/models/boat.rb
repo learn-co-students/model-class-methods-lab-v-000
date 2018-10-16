f0dee9ce8a4d35f71ebe3336540948012472cefa
Christopher P. Dundon <chris.dundon@gmail.com>
2018-10-16 01:52:30 -0400

    lab complete

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,42 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+	def self.first_five
+		self.all.limit(5)
+	end
+
+	def self.dinghy
+		self.where('length < ?', 20)
+	end
+
+	def self.ship
+		self.where('length >= ?', 20)
+	end
+
+	def self.last_three_alphabetically
+		self.order(name: :desc).limit(3)
+	end
+
+	def self.without_a_captain
+		self.where(captain: nil)
+	end
+	
+	def self.sailboats		
+		self.classifications.where(classifications: {name: :Sailboat})
+	end
+
+	def self.catamarans
+		self.classifications.where(classifications: {name: :Catamaran})
+	end
+
+	def self.with_three_classifications
+		self.classifications.group(:'boats.name').having('count(*) == 3')
+	end
+
+private
+
+	def self.classifications
+		self.joins(:classifications)
+	end	
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,32 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+	has_many :classifications, through: :boats
+
+	def self.catamaran_operators
+		#Boat.catamarans.joins(:captain).select(:'captains.name')
+		self.classifications.where(classifications: {name: :Catamaran}).distinct
+	end
+	
+	def self.sailors
+		self.classifications.where(classifications: {name: :Sailboat}).distinct
+	end
+	
+	def self.talented_seafarers
+		motor_ = self.classifications.where(classifications: {name: :Motorboat}).distinct
+		m_hash = {:'captains.name' => motor_.pluck(:name)}
+ 
+		self.sailors.where(m_hash)
+	end
+
+	def self.non_sailors
+		sail_capt = {:'captains.name' => self.sailors.pluck(:name)}
+
+		self.classifications.where.not(sail_capt).distinct
+	end
+
+private
+	def self.classifications
+		self.joins(:classifications)
+	end
+
 end
