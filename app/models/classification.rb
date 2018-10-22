1cd98f7104e564a3708654b9f4a033e8db99a568
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-10-21 23:32:50 -0400

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    # binding.pry
+    all
+  end
+
+  def self.longest
+    joins(:boats).order(Boat.arel_table[:length].desc).uniq.limit(2)
+  end
 end
