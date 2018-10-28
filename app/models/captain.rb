5f6acff3c769b32c7f7b4c663c657e1559234f72
emanuelbierman <emanuel.bierman@gmail.com>
2018-10-28 13:04:33 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,21 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    # all.select do |c|
+      # c.boats.catamarans
+    # end
+    
+      includes(boats: :classifications).where(classifications: { name: 'Catamaran' })
+    # end
+  end
+
+  def self.sailors
+  end
+
+  def self.talented_seafarers
+  end
+
+  def self.non_sailors
+  end
 end
