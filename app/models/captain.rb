35ff5de3f85e2bea8e52b872e35bb39efb0d1e45
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-11-01 11:43:14 -0700

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    Captain.includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  # ONE WAY TO DO IT IN SQLITE (NOT PRECISELY THE SAME AS ABOVE):
+  # SELECT captains.name
+  # FROM boats
+  # INNER JOIN boat_classifications ON boat_classifications.boat_id = boats.id
+  # INNER JOIN captains ON boats.captain_id = captains.id
+  # WHERE boat_classifications.classification_id = 3
+
+
+  def self.sailors
+    Captain.includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def self.talented_seafarers
+    Captain.includes(boats: :classifications).where(classifications: {name: "Motorboat", name: "Sailboat"}).uniq
+  end
+
 end
