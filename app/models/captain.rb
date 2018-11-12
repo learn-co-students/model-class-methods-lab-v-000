a69f1243f693eb5ff444e8dbe526968fcbf1ff77
Daniel <bluh@outlook.com>
2018-11-12 16:04:00 -0500

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,7 +1,11 @@
 class Captain < ActiveRecord::Base
   has_many :boats
-
+  has_many :classifications, through: :boats
   def self.catamaran_operators
-    self.joins(:boats).where(:classifications => {name: "Catamaran"})
+    self.joins(:classifications).where(:classifications => {name: "Catamaran"})
+  end
+
+  def self.sailors
+    self.joins(:classifications).where(:classifications => {name: "Sailboat"}).uniq
   end
 end
