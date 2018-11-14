dc53e04f0fdddf19f16fddbcf26b79c938af8c8f
freeWillee <will.i.lee@outlook.com>
2018-11-14 15:47:20 -0700

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,10 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    #returns an array of the first five boat objects
+    
+  end
+
 end
