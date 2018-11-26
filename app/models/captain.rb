368454100b6dd53a68f13bca1d4808a859391a81
Brian Birdwell <bbirdwell@gmail.com>
2018-11-26 04:55:42 +0000

    complete catamaran method

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,8 @@
+require 'pry'
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    Captain.joins(boats: :classifications).group('captain_id').where(classifications: {name: ["Catamaran"]})
+  end
 end
