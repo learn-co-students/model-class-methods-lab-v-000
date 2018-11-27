1293310af361711cf49735f08c3ab5bda5ab3232
Brian Birdwell <bbirdwell@gmail.com>
2018-11-27 03:12:54 +0000

    complete captain spec

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -8,6 +8,17 @@ class Captain < ActiveRecord::Base
 
   def self.sailors
     Captain.joins(boats: :classifications).group('captain_id').where(classifications: {name: ["Sailboat"]})
+  end
 
+  def self.motorboaters
+    Captain.joins(boats: :classifications).group('captain_id').where(classifications: {name: ["Motorboat"]})
+  end
+
+  def self.talented_seafarers
+    Captain.where("id IN (?)", self.sailors.pluck(:id) & self.motorboaters.pluck(:id))
+  end
+
+  def self.non_sailors
+    where.not("id IN (?)", self.sailors.pluck(:id))
   end
 end
