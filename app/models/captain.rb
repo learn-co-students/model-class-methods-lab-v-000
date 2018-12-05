9799638df5163f00b986987c2d3b9201deebe7c4
Eric Fiorani <efiorani305@gmail.com>
2018-12-05 15:01:05 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,23 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  
+  def self.catamaran_operators
+    self.joins(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    self.joins(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+  end
+
+  def self.motorboaters
+    self.joins(boats: :classifications).where(classifications: {name: "Motorboat"}).distinct
+  end
+
+  def self.talented_seafarers
+    self.where("id IN (?)", self.sailors.pluck(:id) & self.motorboaters.pluck(:id))
+  end
+
+  def self.non_sailors
+    self.where.not("id IN (?)", self.sailors.pluck(:id))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ ArelLab::Application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
+  # Examp  le re source route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
   # Example resource route with options:
