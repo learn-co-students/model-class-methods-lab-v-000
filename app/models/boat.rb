462b46980903a6d90ed9d1c51e8fbb068e2ad950
github username <github email address>
2018-12-09 16:06:07 -0500

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,39 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    Boat.limit(5)
+  end
+
+  def self.dinghy
+    Boat.where("length < '20'")
+  end
+
+  def self.ship
+    Boat.where("length >= '20'")
+  end
+
+  def self.last_three_alphabetically
+    Boat.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    Boat.where(captain_id: nil)
+  end
+
+  def self.sailboats
+    Boat.includes(:classifications).where(classifications: { name: 'Sailboat' })
+  end
+
+  def self.with_three_classifications
+    # binding.pry
+    # Boat.includes(:classifications).where(classifications: { name: 'Sailboat' })
+    Boat.joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+    # .group("classifications.name" )
+  end
+
+  def self.longest_boat
+    Boat.order('length DESC').first
+  end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,30 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    Captain.includes(boats: :classifications).where(classifications: {name: 'Catamaran'})
+  end
+
+  def self.sailors
+    Captain.includes(boats: :classifications).where(classifications: {name: 'Sailboat'}).uniq
+  end
+
+  def self.motorboat_operators
+    Captain.includes(boats: :classifications).where(classifications: {name: 'Motorboat'}).uniq
+  end
+
+
+  def self.talented_seafarers
+    Captain.where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id) )
+    # Captain.includes(boats: :classifications).where(self.motorboat_operators && self.sailors)
+    # Captain.includes(boats: :classifications).where(classifications: {name: 'Motorboat'}).where(classifications: {name: 'Sailboat'}).uniq
+# Model.where(:column => ["value", "other_value"]
+
+  end
+
+  def self.non_sailors
+    Captain.where.not("id IN (?)",self.sailors.pluck(:id))
+    # Captain.includes(boats: :classifications).where(classifications: {name: !'Sailboat'})
+  end
+
 end
