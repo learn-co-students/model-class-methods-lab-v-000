e2d9cfd65a44198713747bd2364a0a348d0fef90
Jeremy <jeremy.emata@gmail.com>
2018-12-12 15:19:35 -0500

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -6,6 +6,20 @@ class Captain < ActiveRecord::Base
   end
 
   def self.sailors
-   includes(boats: :classifications).where(classifications: { name: 'Sailboat' }).uniq 
+   includes(boats: :classifications).where(classifications: { name: 'Sailboat' }).uniq
   end
+
+  def self.talented_seafarers
+   #retrieving values that satisfy one or another condition.  Really just taking the other two and putting together with pluck
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
+  end
+
+  def self.motorboat_operators
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+  end
+
+  def self.non_sailors
+  where.not("id IN (?)", self.sailors.pluck(:id))
+end
+
 end
