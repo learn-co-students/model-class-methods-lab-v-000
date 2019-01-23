501d2ed3ad1898a1ac5c84e838fa1d8609c195b3
github username <github email address>
2019-01-23 14:24:53 -0600

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+ def self.my_all
+   self.all
+ end
+
+ def self.longest
+   boat = Boat.order(length: :desc).first
+   boat.classifications
+ end
+
 end
