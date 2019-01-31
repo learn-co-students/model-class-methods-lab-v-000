2e7a8258586cf75c73b065540834bd30d993c912
Zachary Lehmann <zlehmann@gmail.com>
2019-01-31 19:30:55 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,21 +2,21 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    results = []
-    Captain.all.each do |capt|
-      if capt.boats.joins(boat_classifications: :classification).where('classifications.name' => "Catamaran") != ""
-        results << capt 
-      end 
-    end 
-    results 
+    Captain.joins(boats: :classifications).where('classifications.name' => "Catamaran")
   end
 
-  def self.sailors 
-  end 
+  def self.sailors
+    Captain.joins(boats: :classifications).where('classifications.name' => "Sailboat").group('captains.name')
+  end
 
-  def self.talented_seafarers 
-  end 
+  def self.talented_seafarers
+    array = Captain.joins(boats: :classifications).where('classifications.name' => "Sailboat") &
+    Captain.joins(boats: :classifications).where('classifications.name' => "Motorboat")
+    Captain.where(id: array)
+  end
 
-  def self.non_sailors 
-  end 
+  def self.non_sailors
+    sailors = self.sailors
+    Captain.where.not(id: sailors)
+  end
 end
