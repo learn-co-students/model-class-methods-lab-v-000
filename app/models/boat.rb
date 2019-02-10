e2e08530333452dd8c38ff0ed08fc54ff665dce5
Judy McGoldrick <jjeanius@verizon.net>
2019-02-10 04:40:41 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,34 @@
+require 'pry'
+
 class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    Boat.all.limit(5)
+  end
+
+  def self.dinghy
+    Boat.maximum.(20)length
+  end
+
+  def self.ship
+
+  end
+
+  def self.last_three_alphabetically
+  end
+
+  def self.without_a_captain
+  end
+
+  def self.sailboats
+
+  end
+
+  def self.with_three_classifications
+  end
+
+
 end
