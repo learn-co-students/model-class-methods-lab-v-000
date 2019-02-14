22949a3c70d392b4796d02f85ee620eb1ac42974
maxwellbenton <maxwellbenton@gmail.com>
2019-02-13 19:17:43 -0500

    rails app:update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-ArelLab::Application.config.session_store :cookie_store, key: '_arel-lab_session'
+Rails.application.config.session_store :cookie_store, key: '_arel_lab_session'
