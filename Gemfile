f437a1a861d1b5d8d6ab60eae4665c850a3c3433
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-02-18 01:43:28 -0500

    Complete model class methods

--- Gemfile
+++ Gemfile
@@ -1,10 +1,10 @@
 source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2'
+gem 'rails'
 
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'sqlite3', '~> 1.3.6'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,25 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+  	Captain.joins(boats: [:classifications]).where(classifications: {name: 'Catamaran'})
+  end
+
+  def self.sailors
+  	joins(boats: [:classifications]).where(classifications: {name: 'Sailboat'}).uniq
+  end
+
+  def self.motorboaters
+  	joins(boats: [:classifications]).where(classifications: {name: 'Motorboat'}).uniq
+  end
+
+  def self.talented_seafarers
+  	where("id IN (?)", sailors.pluck(:id) & motorboaters.pluck(:id))
+  end
+
+
+  def self.non_sailors
+  	where.not("id IN (?)", sailors.pluck(:id))
+  end
+
 end
