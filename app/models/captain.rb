a3d4462db6516fcfed9f574b312bd538635ff6d3
Joey Cho <cjy12182000@naver.com>
2019-02-25 13:11:05 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,20 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins(boats: :classifications).where(classifications: {name: 'Catamaran'})
+  end
+
+  def self.sailors
+    joins(boats: :classifications).where(classifications: {name: 'Sailboat'}).uniq
+      end
+
+  def self.talented_seafarers
+    joins(boats: :classifications).group('classifications.name').order('captains.name ASC').where("classifications.name = ? OR classifications.name = ?",'Motorboat','Sailboat')
+  end
+
+  def self.non_sailors
+    where.not(name: self.sailors.pluck(:name))
+  end
+
 end
