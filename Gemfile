a836bdddde5d7fc93698b46a65e779e7e2cf2b75
github username <github email address>
2019-02-28 21:37:44 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.2'
 
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'sqlite3', '~> 1.3.6'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
@@ -36,5 +36,8 @@ group :test, :development do
   gem "rspec-rails"
   gem "factory_girl_rails"
   gem "database_cleaner"
-  gem "pry"
+  gem 'rb-readline'
+  gem 'pry'
+  gem 'pry-coolline'
+
 end
--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,34 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.limit(5)      
+   end
+
+   def self.dinghy
+    self.where("length <' 20'")
+   end
+
+   def self.ship
+    self.where("length >= '20'")
+  end
+
+  def self.last_three_alphabetically
+    self.order('name desc').limit(3)
+  end
+
+  def self.without_a_captain
+    self.where(captain_id: nil)
+  end
+
+  def self.sailboats
+    classification_id_sailboat = Classification.all.where(name: "Sailboat").pluck(:id)
+    BoatClassification.where(classification_id: classification_id_sailboat)                                                                                    
+#find boat_ids where BoatClassification' classification_id = classification_id_sailboat
+    binding.pry
+  end
+
 end
+ # binding.pry
+    # self.order('name DESC')[0 , 3].map {|b|b.name}  
