03f35d271f104a26a54e804c5bbf91e62cd4fef9
Rachel Wyatt <rw126@evansville.edu>
2019-03-01 01:21:50 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,26 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats
+
+  def self.catamaran_operators
+    self.joins(:classifications).where("classifications.name = 'Catamaran'")
+  end
+
+  def self.sailors
+    self.joins(:classifications).where("classifications.name = 'Sailboat'").uniq
+  end
+
+  def self.motors
+    self.joins(:classifications).where("classifications.name = 'Motorboat'").uniq
+  end
+
+  def self.talented_seafarers
+    ids = self.sailors.ids & self.motors.ids
+    self.sailors.where(id: ids)
+  end
+
+  def self.non_sailors
+    ids = self.all.ids - self.sailors.ids
+    self.all.where(id: ids)
+  end
 end
