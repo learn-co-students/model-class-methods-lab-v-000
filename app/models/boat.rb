8b1b04d9ee4216114ab8887eadc46294dff62856
Erin Green <erin.y.green@gmail.com>
2019-03-03 18:14:35 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -16,19 +16,19 @@ class Boat < ActiveRecord::Base
   end
 
   def self.last_three_alphabetically
-
+    all.order(name: :desc).limit(3)
   end
 
   def self.without_a_captain
-    where("captain_id = NIL")
+    where(captain_id: nil)
   end
 
   def self.sailboats
-
+    includes(:classifications).where(classifications: { name: 'Sailboat' })
   end
 
   def self.with_three_classifications
-
+    joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
   end
 
 
