7a3db69aba699c4cde695b12e1f636948e45906a
Erin Green <erin.y.green@gmail.com>
2019-03-03 18:00:54 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,15 +4,15 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-
+    all.limit(5)
   end
 
   def self.dinghy
-
+    where("length < 20")
   end
 
   def self.ship
-
+    where("length >= 20")
   end
 
   def self.last_three_alphabetically
@@ -20,7 +20,7 @@ class Boat < ActiveRecord::Base
   end
 
   def self.without_a_captain
-
+    where("captain_id = NIL")
   end
 
   def self.sailboats
