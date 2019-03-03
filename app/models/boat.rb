f27bc4b9c1717900ab61c4a1ec3ae444b15338a1
Erin Green <erin.y.green@gmail.com>
2019-03-03 17:38:24 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,34 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+
+  end
+
+  def self.dinghy
+
+  end
+
+  def self.ship
+
+  end
+
+  def self.last_three_alphabetically
+
+  end
+
+  def self.without_a_captain
+
+  end
+
+  def self.sailboats
+
+  end
+
+  def self.with_three_classifications
+
+  end
+
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+
+  def self.my_all
+
+  end
+
+  def self.longest
+
+  end
+
 end
