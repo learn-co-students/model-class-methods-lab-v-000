7d91b2083cb7d182df974b68063154b12537edc6
Carley Tripp <carley.tripp@outlook.com>
2019-03-15 02:36:31 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2'
+gem 'rails'
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~>1.3.6'
@@ -34,7 +34,8 @@ end
 
 group :test, :development do
   gem "rspec-rails"
-  gem "factory_girl_rails"
+  # gem "factory_girl_rails"
+  gem "factory_bot_rails"
   gem "database_cleaner"
   gem "pry"
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,19 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: [:classifications]).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    includes(boats: [:classifications]).where(classifications: {name: "Sailboat"}).distinct
+  end
+
+  def self.talented_seafarers
+    includes(boats: [:classifications]).where(classifications: {name: "Motorboat"}, captains: {id: sailors.pluck(:id)})
+  end
+
+  def self.non_sailors
+    where.not(id: sailors.pluck(:id))
+  end
 end
