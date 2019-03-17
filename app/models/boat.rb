8334d96cc0dec07cd79e1b04bfe14f5eacf2aa8d
RonSala <ron.sala@gmail.com>
2019-03-17 09:57:26 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,6 +4,13 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    
+    binding.pry
+    Boat.take(5)
   end
+
+  # private
+
+  # def self.table
+  #   Boat.arel_table
+  # end
 end
