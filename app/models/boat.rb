4c1b8033131f6a6558c3ca1dd40410802ede60eb
lpzraf <lpz.raf@gmail.com>
2019-03-31 21:20:40 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -33,4 +33,8 @@ class Boat < ActiveRecord::Base
     joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
   end
 
+  def self.longest
+   all.order(length: :desc).first
+ end 
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -7,8 +7,19 @@ class Captain < ActiveRecord::Base
   end
 
   def self.sailors
-    # joins(:classifications).where(:classifications => {:name => "Catamaran"})
-    includes(boats: :classifications).where(classifications: { name: 'Sailboat'})
+    includes(boats: :classifications).where(classifications: { name: 'Sailboat'}).distinct
+  end
+
+  def self.motorboat_operators
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+  end
+
+  def self.talented_seafarers
+     where({id: self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id)})
+  end
+
+  def self.non_sailors
+    where.not({ id: self.sailors.pluck(:id)})
   end
 
 end
