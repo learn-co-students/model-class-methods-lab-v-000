a8b012e1dd7b77a8b7573d30e5707552e19606fc
brennanmrose <brennan.m.rose@gmail.com>
2019-04-02 22:21:33 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,49 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+  	first(5)
+  end
+
+  def self.dinghy 
+  	where('length <?', '20')
+  end
+
+  def self.ship
+  	where('length >=?', '20')
+  end
+
+  def self.last_three_alphabetically
+  	order(:name).last(3).reverse
+  end
+
+  def self.without_a_captain
+  	where(captain: nil)
+  end
+
+  def self.sailboats
+  	@sailboat = Classification.find_sailboat
+  	binding.pry
+  end
+
+  def self.with_three_classifications
+
+  end
+
 end
+
+
+  #  describe "::sailboats" do
+  #   it "returns all boats that are sailboats" do
+  #     boats = ["H 28", "Nacra 17", "49er", "Laser", "Harpoon 4.7", "Sunfish"]
+  #     expect(Boat.sailboats.pluck(:name)).to eq(boats)
+  #   end
+  # end
+
+  #  describe "::with_three_classifications" do
+  #   it "returns boats with three classifications" do
+  #     boats = ["Nacra 17", "Zodiac CZ7", "Sun Tracker Regency 254 XP3"].sort
+  #     expect(Boat.with_three_classifications.pluck(:name).sort).to eq(boats)
+  #   end
+  # end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,9 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.find_sailboat
+  	where('name = ?', 'Sailboat')
+  end
+
 end
