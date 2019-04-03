9a03195a2bc5f546d74f912a94c9a1751f21f339
ricecake88 <vocalise@gmail.com>
2019-04-02 23:09:16 -0700

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,24 +2,40 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    captains = []
-    boats = Classification.select_boats("Catamaran")
-    boats.each do |boat|
-      captains << boat.captain
-    end
-    captains
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
   end
 
+  #def self.catamaran_operators
+  #  captains = []
+  #  boats = Classification.select_boats("Catamaran")
+  #  boats.each do |boat|
+  #    captains << boat.captain
+  #  end
+  #  captains
+  #end
+
   def self.sailors
-    captains = []
-    boats = Classification.select_boats("Sailboat")
-    boats.each do |boat|
-      if !boat.captain.nil?
-        captains << boat.captain
-      end
-    end
-    captains.uniq!
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
   end
+
+  #def self.sailors
+  #  captains = []
+  #  boats = Classification.select_boats("Sailboat")
+  #  boats.each do |boat|
+  #    if !boat.captain.nil?
+  #      captains << boat.captain
+  #    end
+  #  end
+  #  captains.uniq!
+  #end
+
+  #def self.motorboaters
+  #  includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+  #end
+
+  #def self.talented_seafarers
+  #  where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
+  #end
 
   def self.talented_seafarers
     sailboat_captains = []
@@ -36,15 +52,19 @@ class Captain < ActiveRecord::Base
   end
 
   def self.non_sailors
-    sailors = Captain.sailors
-    captains = []
-    Captain.all.each do |cap|
-      if !Captain.sailors.include?(cap)
-        captains << cap
-      end
-    end
-    captains
+    where.not("id IN (?)", self.sailors.pluck(:id))
   end
+
+  #def self.non_sailors
+  #  sailors = Captain.sailors
+  #  captains = []
+  #  Captain.all.each do |cap|
+  #    if !Captain.sailors.include?(cap)
+  #      captains << cap
+  #    end
+  #  end
+  #  captains
+  #end
 
 
 end
