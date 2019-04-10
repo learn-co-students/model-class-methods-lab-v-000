0749a9c8810fb2666d166a8593576155e62aa59a
Leslie Stevens <flymaven@protonmail.com>
2019-04-10 17:21:15 -0500

    build method

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,21 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    boats = Boat.select("boats.*").includes(:classifications).where(classifications: {name: "Catamaran"})
+    boats.map do |boat|
+      boat.captain
+    end
+    #Captain.joins(:boats).includes(:classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  #def self.sailboats
+    #boats = Boat.arel_table
+    #Boat.select("boats.*").includes(:classifications).where(classifications: {name: "Sailboat"})
+  #end
+
+#  def self.with_three_classifications
+  #  Boat.joins(:classifications).group("boat_id").having("COUNT(*) = 3")
+#  end
+
 end
