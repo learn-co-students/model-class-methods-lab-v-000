c525d45d21cb12463f1eced94fe7fc58fd9af217
Heather Miller <heather.miller114@gmail.com>
2019-04-13 21:44:29 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+  end
+
+  def self.talented_seafarers
+    c = includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+    d = includes(boats: :classifications).where(classifications: {name: "Sailboat"})
+    a = c + d 
+    a.select { |z| a.count(z) > 1 }.uniq
+
+  end
+
+  def self.non_sailors
+    self.all - self.sailors
+  end
 end
