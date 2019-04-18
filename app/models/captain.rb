7b395d1b92b201c5df16ab31ecfd553505312373
Ashley <ashleywong678@gmail.com>
2019-04-18 14:13:13 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -9,16 +9,16 @@ class Captain < ActiveRecord::Base
     joins(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
   end
 
-  def self.motorboats
+  def self.motorboat_operators
     joins(boats: :classifications).where(classifications: {name: "Motorboat"}).uniq
   end
 
   def self.talented_seafarers
-    where(self.sailors && self.motorboats)
+    where("id IN (?)", self.sailors & self.motorboat_operators)
   end
 
   def self.non_sailors
-
+    where.not("id IN (?)", self.sailors.pluck(:id))
   end
   
 end
