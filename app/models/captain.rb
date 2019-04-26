3cd10dfec93bdaa7ff7b7971cdd868d4c7029a1a
sarawall1187 <sarawall1187@gmail.com>
2019-04-26 14:40:53 -0700

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,11 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+  end
 end
