6d6c42fc68c07eb5537ae2ce7f06d0c53d1e5667
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-30 19:03:36 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -18,6 +18,6 @@ class Captain < ActiveRecord::Base
   end
 
   def self.non_sailors
-    Captain.joins(:classifications).where.not(classifications: {name: "Sailboat"}).distinct
+    Captain.where.not("id IN (?)", self.sailors)
   end
 end
