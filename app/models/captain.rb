c8cadcaf010c7f4ce61f1d211379d947e3a2e5c8
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-30 18:53:48 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -9,6 +9,10 @@ class Captain < ActiveRecord::Base
     Captain.joins(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
   end
 
+  def self.sailors
+    Captain.joins(boats: :classifications).where(classifications: {name: "Motorboat"}).distinct
+  end
+
   def self.talented_seafarers
     self.sailors.joins(boats: :classifications).where(classifications: {name: "Motorboat"})
   end
