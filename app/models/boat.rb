f540d5d35bdafb20ccdcf4e95fa3d099c63e9626
RonSala <ron.sala@gmail.com>
2019-04-29 22:11:53 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -28,6 +28,6 @@ class Boat < ActiveRecord::Base
   end
 
   def self.with_three_classifications
-    
+    where(:classifications.size == 3)
   end
 end
--- app/models/boat_classification.rb
+++ app/models/boat_classification.rb
@@ -1,8 +1,4 @@
 class BoatClassification < ActiveRecord::Base
   belongs_to :boat
   belongs_to :classification
-
-  # def self.sailboats
-  #   where("name = ?", "Sailboat")
-  # end
 end
