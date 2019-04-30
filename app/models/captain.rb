434108895f824c5fe07f3a81fe689180cda2951a
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-30 19:04:41 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -18,6 +18,6 @@ class Captain < ActiveRecord::Base
   end
 
   def self.non_sailors
-    Captain.where.not("id IN (?)", self.sailors)
+    Captain.where.not("id IN (?)", self.sailors.pluck(:id))
   end
 end
