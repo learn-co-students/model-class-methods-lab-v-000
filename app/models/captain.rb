16c0aca31b6d0e8ed1f7bc924845c7e72e3bb756
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-30 18:43:06 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,7 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    Captain.join(boat: :classifications).where(classifications: {name: "Catamaran"})
+  end
 end
