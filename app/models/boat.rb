ee7903b302f814d2ea0b9dff4f67dbd57d0bfcc5
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-30 18:20:10 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -25,7 +25,7 @@ class Boat < ActiveRecord::Base
 
 
   def self.sailboats
-    Boat.joins(:classifications).where(classifications: {name: "sailboat"})
+    Boat.joins(:classifications).where(classifications: {name: "Sailboat"})
   end
 
   def self.with_three_classifications
