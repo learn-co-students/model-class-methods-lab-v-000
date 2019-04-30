3fe7d3982f5200ca61115c6f204e1212c737acdf
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-30 19:02:17 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -18,6 +18,6 @@ class Captain < ActiveRecord::Base
   end
 
   def self.non_sailors
-    Captain.joins(boats: :classifications).where.not(classifications: {name: "Sailboat"})
+    Captain.joins(:classifications).where.not(classifications: {name: "Sailboat"})
   end
 end
