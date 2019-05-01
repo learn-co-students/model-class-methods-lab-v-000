048b2392b25aa86331baa15c1ad4b39c86f85f4a
AlwinaO <aoyewole2586@gmail.com>
2019-04-30 22:28:07 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -6,11 +6,21 @@ class Boat < ActiveRecord::Base
 
   def self.first_five
     first(5)
-
   end
 
   def self.dinghy
-    where("length" < 20)
+    where("length < ?",  20)
+  end
 
+  def self.ship
+    where("length >= ?",  20)
+  end
+
+  def self.last_three_alphabetically
+    order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    where.not(:captain_id).pluck(:name)
   end
 end
