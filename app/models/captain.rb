0f03b56287c1b336408cd78aa7abe026a3fa3f77
Meg Gutshall <meghan.gutshall@gmail.com>
2019-05-03 20:20:11 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,19 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    where(boat.name = catamaran)
+  end
+
+  def self.sailors
+    where(boat.classifications.name = sailboat)
+  end
+
+  def self.talented_seafarers
+    # have motorboat and sailboat
+  end
+
+  def self.non_sailors
+    # don't have sailboat
+  end
 end
