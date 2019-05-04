7b3dc10cbe12273a62b01fab48d461c1455e0360
AlwinaO <aoyewole2586@gmail.com>
2019-05-03 23:08:21 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,7 +1,7 @@
 class Captain < ActiveRecord::Base
   has_many :boats
 
-# call the boats table then the column classifications because of the has_many through association then call the classifications table and name column
+# call the boats table then the column classifications (because of the has_many through association) then call the classifications table and name column
   def self.catamaran_operators
     includes(boats: :classifications).where(classifications: {:name => "Catamaran"})
   end
