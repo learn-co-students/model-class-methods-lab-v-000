8f211a4a2e1f2f9c1108f301a0f5e9cd6d970c5d
AlwinaO <aoyewole2586@gmail.com>
2019-05-04 22:55:36 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -5,4 +5,20 @@ class Captain < ActiveRecord::Base
   def self.catamaran_operators
     includes(boats: :classifications).where(classifications: {:name => "Catamaran"})
   end
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {:name => "Sailboat"}).distinct
+  end
+
+  def self.motorboats
+    includes(boats: :classifications).where(classifications: {:name => "Motorboat"})
+  end
+
+  def self.talented_seafarers
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboats.pluck(:id))
+  end
+
+  def self.non_sailors
+    where.not("id IN (?)", self.sailors)
+  end
 end
