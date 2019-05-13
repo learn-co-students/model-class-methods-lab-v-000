14c410a5ec3cb2c65df4ec031302dc17cdf2b453
RonSala <ron.sala@gmail.com>
2019-05-12 23:29:12 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -28,6 +28,6 @@ class Boat < ActiveRecord::Base
   end
 
   def self.with_three_classifications
-    where(:classifications.size == 3)
+    joins(:classifications).group("boats.id").having("COUNT(*) = 3")
   end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,12 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats
+
+  def self.catamaran_operators
+    joins(:boats).joins(:classifications).where(classifications: { name: 'Catamaran' }).distinct
+  end
+
+  def self.sailors
+    joins(:boats).joins(:classifications).where(classifications: { name: 'Sailboat' }).distinct
+  end
 end
