ca5830a04ae6775b472f9648e504215956d027af
github username <github email address>
2019-05-16 18:18:01 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,18 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    #Boat.find(:all, :order => "boat_id", :limit => 5)
+    Boat.first(5)
+  end
+
+  def self.dinghy
+    Boat.where( "length < ?", 20)
+  end
+
+  def self.ship
+    Boat.where( "length > ?", 20)
+  end
+
 end
