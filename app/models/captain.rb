3023fc5527008faac54a0661e69b7072f3e7b7df
Jason Mei <jason.mei403@gmail.com>
2019-05-18 16:32:31 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,16 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.all.select do |captain|
+      captain.boats.each do |boat|
+        boat.classifications.find_by(name: 'Catamaran')
+      end
+    end
+  end
+
+  def self.sailors
+
+  end
+
 end
