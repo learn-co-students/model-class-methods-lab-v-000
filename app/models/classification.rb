123192e429aea6b4c5929f77dbd2cda221af8102
RonSala <ron.sala@gmail.com>
2019-05-19 22:24:34 -0400

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    where.not(name: nil)
+  end
+
+  def self.longest
+    binding.pry
+    where(id: Boat.longest)
+  end
 end
