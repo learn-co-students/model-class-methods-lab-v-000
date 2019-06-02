5ffedca5eebee69b10ce2be13ec2a1fb41c0ae90
Srija S <srijakumar7@gmail.com>
2019-06-02 21:19:40 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -8,12 +8,29 @@ def self.first_five
   all.limit(5)
 end
 
-def dinghy
-  where ("length<20")
+def self.dinghy
+  where ("length < 20")
 end
 
 def self.ship
-
+  where ("length >= 20")
 end
+
+def self.last_three_alphabetically
+  all.order(name: :desc).limit(3)
+end
+
+def self.without_a_captain
+  where(captain_id: nil)
+end
+
+def self.sailboats
+  includes(:classifications).where(classifications: {name: 'Sailboat'})
+end
+
+def self.with_three_classifications
+  joins(:classifications).group("boats.id").having("COUNT(*) = 3")
+end
+
 
 end
