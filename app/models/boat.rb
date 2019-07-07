1ecf6d61350fd7939152d1aa689191735a590e46
aljimenez5 <arvida.jimenez@gmail.com>
2019-07-06 22:25:19 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,42 +4,41 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    # all.limit(5)
+    self.first(5)
   end
 
   def self.dinghy
-    # where("length < 20")
+    self.where('length < ?', 20)
   end
 
   def self.ship
-    # where("length >= 20")
+    self.where('length >= ?', 20)
   end
 
   def self.last_three_alphabetically
-    # all.order(name: :desc).limit(3)
+    self.order(name: :desc).first(3)
   end
 
   def self.without_a_captain
-    # where(captain_id: nil)
+    self.where(captain_id: nil)
   end
 
   def self.sailboats
-    # includes(:classifications).where(classifications: { name: 'Sailboat' })
+    self.includes(:classifications).where(classifications: { name: 'Sailboat' })
   end
 
   def self.with_three_classifications
-    # This is really complex! It's not common to write code like this
-    # regularly. Just know that we can get this out of the database in
-    # milliseconds whereas it would take whole seconds for Ruby to do the same.
-    #
-    # joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+    self.includes(:classifications).group("boats.id").having("Count(*) = 3")
+
   end
 
   def self.non_sailboats
+    self.includes(:classifications).where.not(classifications: { name: 'Sailboat' })
+
     # where("id NOT IN (?)", self.sailboats.pluck(:id))
   end
 
   def self.longest
-    # order('length DESC').first
+    self.order(length: :desc).first
   end
 end
