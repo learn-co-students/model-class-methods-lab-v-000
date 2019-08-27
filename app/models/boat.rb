505aa49bdac0aa220da63b323ec4a2425f107ecc
David Chiu <davidwchiu@gmail.com>
2019-08-27 00:25:44 +0000

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,7 +4,7 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    # all.limit(5)
+    all.limit(5)
   end
 
   def self.dinghy
